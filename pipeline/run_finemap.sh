9636b28478fabf1ff74403482a179dd3a8d420e4
teresa_sansan <joygirlsansan@gmail.com>
2021-09-16 14:18:24 -0400

    kunkle and bellenguez w. all+2 annotations

--- pipeline/run_finemap.sh
+++ pipeline/run_finemap.sh
@@ -3,11 +3,11 @@
 for i in {1..22}
 do
         echo "run chr$i" 
-        sbatch --export=chr=$i finemap_kunkle.sh 
-       # sbatch --export=chr=$i,max_num_snp=3 finemap_kunkle_multi.sh 
-       # sbatch --export=chr=$i,max_num_snp=5 finemap_kunkle_multi.sh 
-       # sbatch --export=chr=$i,max_num_snp=7 finemap_kunkle_multi.sh 
-       # sbatch --export=chr=$i,max_num_snp=10 finemap_kunkle_multi.sh 
+       # sbatch --export=chr=$i polyfun_finemap_bellenguez.sh 
+        sbatch --export=chr=$i,max_num_snp=3 finemap_bellenguez_multi.sh 
+        sbatch --export=chr=$i,max_num_snp=5 finemap_bellenguez_multi.sh 
+        sbatch --export=chr=$i,max_num_snp=7 finemap_bellenguez_multi.sh 
+        sbatch --export=chr=$i,max_num_snp=10 finemap_bellenguez_multi.sh 
 
 done
 
