f55f0a07a64c82e8604b518e61e13990a4c92b48
teresa_sansan <joygirlsansan@gmail.com>
2021-09-24 15:17:56 -0400

    fix chr1 finemap_timeout error

--- pipeline/aggregate_finemap_result.sh
+++ pipeline/aggregate_finemap_result.sh
@@ -1,7 +1,21 @@
 cd /gpfs/commons/home/tlin/polyfun_omer_repo
 
-python aggregate_finemapper_results_modified.py \
-	--out-prefix /gpfs/commons/home/tlin/output/kunkle_all/finemap/all_anno \
-	--sumstats /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/kunkle_2019/AD_Kunkle_etal_Stage1.parquet \
-	--out ~/output/kunkle_all/finemap/aggregrate.all.txt.gz \
-	--allow-missing
+kunkle='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/kunkle_2019/AD_Kunkle_etal_Stage1.parquet'
+bellenguez='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/Bellenguez_2021_stage1.parquet'
+path='/gpfs/commons/home/tlin/output/kunkle_all_2/finemap/max_snp_1'
+prefix='all_anno'
+
+for chr in {1..22}
+do
+	python aggregate_finemapper_results_modified.py \
+		--out-prefix $path/$prefix \
+		--sumstats $kunkle \
+		--out $path/chr${chr}.aggregrate.all.txt.gz \
+		--allow-missing \
+       		--chr $chr
+done
+
+
+#prefix='/gpfs/commons/home/tlin/output/kunkle_all/finemap/all_anno'
+#output='~/output/kunkle_all/finemap/aggregrate.all.txt.gz'
+#prefix='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/finemap/max_snp_1/finemap_bellenguez_all_2'
