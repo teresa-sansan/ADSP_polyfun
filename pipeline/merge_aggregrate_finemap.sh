a628db34e02fb6544f1434b0d2c1299d4f5440e7
teresa_sansan <joygirlsansan@gmail.com>
2021-10-01 17:18:00 -0400

    aggregrate & polypred

--- pipeline/merge_aggregrate_finemap.sh
+++ pipeline/merge_aggregrate_finemap.sh
@@ -1,13 +1,14 @@
-cd /gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/finemap_snpvar_constrained/max_snp_10
-
-zcat chr1.aggregrate.all.txt.gz| head -n 1 > aggregrate.all.txt
-for i in {1..22}
-do
-   echo merging chr $i
-   zcat chr${i}.aggregrate.all.txt.gz |grep -v CHR >> aggregrate.all.txt
+for max_snp in 1 3 5 7 
+do 
+  cd /gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/finemap_snpvar_constrained/max_snp_${max_snp}
+  zcat chr1.aggregrate.all.txt.gz| head -n 1 > aggregrate.all.txt
+  for i in {1..22}
+  do
+     echo merging chr $i
+     zcat chr${i}.aggregrate.all.txt.gz |grep -v CHR >> aggregrate.all.txt
+  done
+  echo
+  echo zipping the file....
+  gzip aggregrate.all.txt 
+  echo finished, total line = $( zcat aggregrate.all.txt | wc -l )
 done
-echo
-echo zipping the file....
-gzip aggregrate.all.txt 
-echo finished, total line = $( zcat aggregrate.all.txt | wc -l )
-
