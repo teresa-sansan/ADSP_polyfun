0ac0efaa75a851fcc0155359770271094e10250e
Teresa Lin <tlin@nygenom.org>
2021-10-12 10:44:53 -0400

    rescue_finemap_susie

---
+++ pipeline/check_finishing_finemap.sh
@@ -0,0 +1,29 @@
+path="/gpfs/commons/home/tlin/output/bellenguez/bellenguez_bl/finemap_susie"
+prefix=finemap_bellenguez_susie
+touch $path/check_finemap.txt   ##create file
+
+function check
+{
+for i in {1..22}
+do
+  export gz=$(ls $1.$i.*|grep -v log|wc -l)   
+  export log=$(ls $1.$i.*|grep log|wc -l)
+  if [ $gz -ne $log ] ;then 
+    echo chr$i  gz=$gz log=$log
+  fi
+done
+echo
+}
+
+for i in 1 3 5 7 10
+do 
+	cd ${path}/max_snp_$i
+       	echo $prefix/max_snp_${i}| tee -a $path/check_finemap.txt 
+	check $prefix| tee -a $path/check_finemap.txt 
+done
+
+
+#path='/gpfs/commons/home/tlin/output/kunkle_all/finemap_overlap/finemap_max_snp_'
+#path='/gpfs/commons/home/tlin/output/kunkle_all/finemap'
+#path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/finemap_snpvar_constrained'
+path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/finemap_snpvar_constrained'
