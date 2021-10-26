550da99a27b03a081a037e95d3d62aedc576142a
Teresa Lin <tlin@nygenom.org>
2021-10-26 16:35:16 -0400

    track, up to date

---
+++ extract_finemap_agg.sh
@@ -0,0 +1,24 @@
+prefix='finemap_bellenguez_susie'
+path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_bl/finemap_susie'
+
+#prefix='finemap_bellenguez_all_2'
+#path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/finemap_snpvar_constrained'
+agg='aggregrate.all.txt.gz'
+
+echo $path
+
+sig_level='1e-3'
+for i in 1 3 5 7 10
+do	
+	## extract column name
+	zcat ${path}/max_snp_$i/$agg | head -n 1 > ${path}/max_snp_$i/$prefix.extract_${sig_level}.csv 	
+	echo start max_snp_$i ## extract those that reach GWAS significant level
+	zcat ${path}/max_snp_$i/$agg | awk '$10< 1e-3 {print$0}' >> ${path}/max_snp_$i/$prefix.extract_${sig_level}.csv 
+	#echo removing duplicated SNPs ....
+	#cat ${path}/max_snp_$i/$prefix.extract_e-01.csv | awk '!a[$2]++' > ${path}/max_snp_$i/all_anno_extract_uniq_e-01.csv
+	echo start zipping the file
+	gzip ${path}/max_snp_$i/$prefix.extract_${sig_level}.csv 
+
+done 
+
+
---
+++ pipeline/run_aggregate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in 1 3 5 7 10
+do
+        echo "max_snp = $i" 
+        sbatch --export=max_snp=$i aggregate_finemap_result.sh 
+
+done
+
