a6281e651db92efb932108fba6e5a083e2bd0591
Teresa Lin <tlin@nygenom.org>
2021-11-05 10:04:09 -0400

    check finemap_bellenguez

--- pipeline/run_finemap_rescue.sh
+++ pipeline/run_finemap_rescue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-sbatch finemap_rescue_snp1.sh        
+#sbatch finemap_rescue_snp1_all_2.sh  
 sbatch --export=max_snp=3 finemap_rescue.sh 
 sbatch --export=max_snp=5 finemap_rescue.sh 
 sbatch --export=max_snp=7 finemap_rescue.sh 
