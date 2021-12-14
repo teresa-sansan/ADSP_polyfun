6b1beafee346621abef371ca4ee60de06b65ff73
teresa_sansan <joygirlsansan@gmail.com>
2021-12-14 15:31:00 -0500

    fix the error of NAs when loading into R studio

--- merge_polypred_phenotype.py
+++ merge_polypred_phenotype.py
@@ -1,61 +1,40 @@
+## Author: Teresa Lin
+## Dec 14, 2021
+
 import pandas as pd
 import numpy as np
 
-#polypred_max1 = pd.read_csv("/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/polypred/PLINKupdate_max1.prs",sep='\t')
-#polypred_max3 = pd.read_csv("/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/polypred/PLINKupdate_max3.prs",sep='\t')
-#polypred_max5 = pd.read_csv("/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/polypred/PLINKupdate_max5.prs",sep='\t')
-#polypred_max7 = pd.read_csv("/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/polypred/PLINKupdate_max7.prs",sep='\t')
+polypred_max1 = pd.read_csv("/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/polypred/PLINKupdate_max1.prs",sep='\t')
+polypred_max3 = pd.read_csv("/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/polypred/PLINKupdate_max3.prs",sep='\t')
+polypred_max5 = pd.read_csv("/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/polypred/PLINKupdate_max5.prs",sep='\t')
+polypred_max7 = pd.read_csv("/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/polypred/PLINKupdate_max7.prs",sep='\t')
 polypred_max10 = pd.read_csv("/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/polypred/PLINKupdate_max10.prs",sep='\t')
-phenotype_chirag = pd.read_csv("/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/phenotype_data_10_28_2021/all_phenotypes_unique_ancestry_all_all.tsv", sep='\t')
+#phenotype_chirag = pd.read_csv("/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/phenotype_data_10_28_2021/all_phenotypes_unique_ancestry_all_all.tsv", sep='\t')
 phenotype_chirag_noNA = pd.read_csv("/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/phenotype_data_10_28_2021/all_phenotypes_unique_ancestry_subset.tsv", sep = '\t')
 
 
 print(phenotype_chirag_noNA.shape)
+#print("check if there are NAs")
+#print(phenotype_chirag_noNA.isna().sum())
 
-
-def combine_phenotype_polypred(polypred, phenotype = phenotype_chirag_noNA):
+def combine_phenotype_polypred(polypred, savename, phenotype = phenotype_chirag_noNA):
     input_shape = phenotype.shape
-    polypred=phenotype.merge(polypred, right_on = "IID", left_on = "SampleID").drop(["IID"],axis=1)
-    polypred=polypred.loc[:,["SUBJID","SampleID","PRS","AD_status_final","Sex","age_covariate","APOE","Race","Ethnicity","final_population","X1","X2","X3","X4","X5","X6","X7","X8","X9","X10","X11","X12","X13","X14","X15","X16","X17","X18","X19","X20"]]
-    polypred = polypred.rename(columns={"AD_status_final":"Diagnosis","age_covariate":"Age"}) 
+    polypred=phenotype.merge(polypred, right_on = "IID", left_on = "SampleID")
+    polypred = polypred.drop(columns = ['FID','IID','flag_age_covariate','Duplicate_SUBJID'],axis = 1)   ## removed those unwanted columns. 
+    polypred = polypred.rename(columns={"AD_status_final":"Diagnosis","age_covariate":"Age"})  ## rename columns for simplification
     print("before merging:%s, after merging:%s"%(input_shape[0],polypred.shape[0]))
-
+    print(polypred.isna().sum())
+    ## because idk why but when leaving them with NAs will generate wrong result when importing to R.
+    ## So I directly filled them up with -100. 
+    polypred['Race'] = polypred['Race'].replace(np.nan, -100)
+    polypred['Ethnicity'] = polypred['Ethnicity'].replace(np.nan, -100)
+    polypred.to_csv(savename, index=False, sep ='\t')
+    print("save to " , savename)
     return(polypred)
 
 
-# def combine_phenotype_polypred(polypred, phenotype = phenotype_chirag_noNA):
-#     input_shape = polypred.shape
-#     polypred=polypred.merge(phenotype, left_on = "IID", right_on = "SampleID").drop(["IID"],axis=1)
-#     polypred=polypred.loc[:,["SUBJID","SampleID","PRS","AD_status_final","Sex","age_covariate","APOE","Race","Ethnicity","final_population","X1","X2","X3","X4","X5","X6","X7","X8","X9","X10","X11","X12","X13","X14","X15","X16","X17","X18","X19","X20"]]
-#     polypred = polypred.rename(columns={"AD_status_final":"Diagnosis","age_covariate":"Age"}) 
-#     print("before merging:%s, after merging:%s"%(input_shape[0],polypred.shape[0]))
-
-#     return(polypred)
-
-
-
-
-
-
-#max1 = combine_phenotype_polypred(polypred_max1)
-#max3 = combine_phenotype_polypred(polypred_max3)
-#max5 = combine_phenotype_polypred(polypred_max5)
-#max7 = combine_phenotype_polypred(polypred_max7)
-max10 = combine_phenotype_polypred(polypred_max10)
-
-
-##because idk why but direcly merging them together will generate strange values ( they probably just randomly filled up the NAs)
-
-max10['Race'] = max10['Race'].replace(np.nan, -100)
-max10['Ethnicity'] = max10['Ethnicity'].replace(np.nan, -100)    
-
-#max1.to_csv("/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_1_subset.tsv",index=False, sep = '\t')
-#max3.to_csv("/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_3_subset.tsv",index=False, sep = '\t')
-#max5.to_csv("/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_5_subset.tsv",index=False, sep = '\t')
-#max7.to_csv("/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_7_subset.tsv",index=False, sep = '\t')
-max10.to_csv("/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_10_subset_new.tsv",index=False, sep = '\t')
-
-
-
-
-
+max1 = combine_phenotype_polypred(polypred_max1,"/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_1_subset.tsv")
+max3 = combine_phenotype_polypred(polypred_max3,"/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_3_subset.tsv")
+max5 = combine_phenotype_polypred(polypred_max5,"/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_5_subset.tsv")
+max7 = combine_phenotype_polypred(polypred_max7,"/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_7_subset.tsv")
+max10 = combine_phenotype_polypred(polypred_max10,"/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_10_subset.tsv")
