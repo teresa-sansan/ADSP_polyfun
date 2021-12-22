83e4b709ca44cc0fee1424380661ebace49f2207
Teresa Lin <tlin@nygenom.org>
2021-12-22 10:55:30 -0500

    create polyfun acceptable format

--- create_ma.py
+++ create_ma.py
@@ -1,10 +1,9 @@
 import pandas as pd
-
-bellenguez = pd.read_parquet("/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/Bellenguez_2021_stage1.parquet")
+bellenguez=pd.read_csv("/gpfs/commons/home/tlin/data/bellenguez_2021_final.tsv.gz", compression='gzip', sep='\t')
 bellenguez_19 = pd.read_parquet("/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/bellenguez_2021_stage1_hg19.parquet") 
 
-bellenguez["POS"] = bellenguez['CHR'].astype(str) +'_'+ bellenguez["BP"].astype(str) + '_' + bellenguez["A1"]+ '_' +bellenguez["A2"]
-bellenguez_19["POS"] = bellenguez_19['CHR'].astype(str) +'_'+ bellenguez_19["BP"].astype(str) + '_' + bellenguez_19["Allele1"]+'_'+bellenguez_19["Allele2"]
+bellenguez["POS"] ='chr'+ bellenguez['CHR'].astype(str) +'_'+ bellenguez["BP"].astype(str) + '_' + bellenguez["A1"]+ '_' +bellenguez["A2"]
+bellenguez_19["POS"] = bellenguez_19['CHR'].astype(str) +'_'+ bellenguez_19["BP_37"].astype(str) + '_' + bellenguez_19["Allele1"]+'_'+bellenguez_19["Allele2"]
 bellenguez["POS"] = bellenguez["POS"].astype("string") 
 bellenguez_19["POS"] = bellenguez_19["POS"].astype("string")
 
