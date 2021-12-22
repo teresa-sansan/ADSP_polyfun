e279f0c276d2ed1a01d9e53e27b526f5eed48892
Teresa Lin <tlin@nygenom.org>
2021-12-22 15:05:06 -0500

    PLINK, updateRSID & debug

--- pipeline/plink_clump.sh
+++ pipeline/plink_clump.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1> /gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/clumping/max_10.log 2>&1
+#SBATCH --job-name=bellenguez_clump
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=tlin@nygenome.org
+#SBATCH --mem=150G
+#SBATCH --time=10:00:00
+#SBATCH --output=/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/clumping/%x_%j.log
 
-for chr in {1..22}
-do
+
+
 echo start chr $chr
-~/plink --bfile ~/data/biallelic/${chr}_filt \
+~/plink \
+--bfile ~/data/biallelic/${chr}_filt \
+--clump-p1 1 \
 --clump-r2 0.1  \
 --clump-kb 250  \
---clump /gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/max_snp_10/chr${chr}.aggregrate.all.txt  \
---clump-snp-field SNP \
---clump-field P \
---out /gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/clumping/clump_max10_chr${chr}
-done
+--clump /gpfs/commons/home/tlin/data/bellenguez_2021_final.tsv.gz \
+--clump-snp-field RSID \
+--clump-field PVALUE \
+--out /gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/clumping/clump_chr${chr}
+
+
+#--clump /gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/max_snp_10/chr${chr}.aggregrate.all.txt  \
