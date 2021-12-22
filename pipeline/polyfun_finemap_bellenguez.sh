944d60258b4fa7d5f94a57a883e0ca188119a3d5
teresa_sansan <joygirlsansan@gmail.com>
2021-12-22 10:56:49 -0500

    rerun finemap with new RSID (bellenguez)

--- pipeline/polyfun_finemap_bellenguez.sh
+++ pipeline/polyfun_finemap_bellenguez.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=150G
 #SBATCH --time=28:00:00
-#SBATCH --output=/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/finemap_susie/max_snp_1/%x_%j.log
+#SBATCH --output=/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/max_snp_1/%x_%j.log
 
 cd /gpfs/commons/home/tlin/polyfun_omer_repo
 
@@ -14,11 +14,11 @@ conda activate polyfun
 echo chr $chr
 FILES="/gpfs/commons/groups/knowles_lab/data/ldsc/polyfun/ukb_ld/chr${chr}*"
 #sumstat="/gpfs/commons/home/tlin/output/bellenguez/bellenguez_bl/bellenguez_bl.${chr}.snpvar_constrained.gz"
-sumstat="/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/bellenguez_all.${chr}.snpvar_ridge_constrained.gz"
+#sumstat="/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/bellenguez_all.${chr}.snpvar_ridge_constrained.gz"
 #sumstat="output/bellenguez/bellenguez_roadmap_deepsea_brain_atac/bellenguez_roadmap_deepsea_brain_atac.${chr}.snpvar_ridge_constrained.gz"
+sumstat="/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/bellenguez_all.${chr}.snpvar_constrained.gz"
 
 
-###susie
 for i in $FILES
 do	
 	#echo $i
@@ -29,12 +29,11 @@ do
 	python finemapper.py \
 		--sumstats $sumstat \
 		--n 487511 \
-		--non-func \
 	  	--chr ${chr} --start $start --end $end \
 	  	--method susie \
      	  	--max-num-causal 1 \
 	  	--allow-missing \
-		--out "/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/finemap_susie/max_snp_1/finemap_bellenguez_susie.${chr}.$start.$end.gz"
+		--out "/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/max_snp_1/finemap_bellenguez.${chr}.$start.$end.gz"
 
 	
 
--- pipeline/run_finemap.sh
+++ pipeline/run_finemap.sh
@@ -3,7 +3,7 @@
 for i in {1..22} 
 do
         echo "run chr$i" 
-        sbatch --export=chr=$i polyfun_finemap_bellenguez.sh 
+        #sbatch --export=chr=$i polyfun_finemap_bellenguez.sh 
         sbatch --export=chr=$i,max_num_snp=3 finemap_bellenguez_multi.sh 
         sbatch --export=chr=$i,max_num_snp=5 finemap_bellenguez_multi.sh 
         sbatch --export=chr=$i,max_num_snp=7 finemap_bellenguez_multi.sh 
