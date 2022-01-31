647cfe3f2a75b98dc445673c578bce7417197e89
Teresa Lin <tlin@nygenom.org>
2022-01-31 10:48:09 -0500

    test IID/FID

--- polypred/merge_beta/create_phenotype.sh
+++ polypred/merge_beta/create_phenotype.sh
@@ -1,1 +1,3 @@
-cat all_phenotypes_unique_ancestry_all_all.tsv | awk '{OFS = "\t"}{print $2 ,$1,$8}'> ADSP_pheno_merge_beta.tsv
+## phenotype file requires (in the order of) FID IID PHENO
+## take SUBJID (#1) SampleID (#2) and AD_status_final(AD diagnosis, #8)
+cat /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/phenotype_data_10_28_2021/all_phenotypes_unique_ancestry_all_all.tsv | awk '{OFS = "\t"}{print $1,$2,$8}'> /gpfs/commons/home/tlin/data/ADSP_pheno_merge_beta_new.tsv
--- polypred/merge_beta/polypred_mergebeta.sh
+++ polypred/merge_beta/polypred_mergebeta.sh
@@ -6,19 +6,15 @@
 #SBATCH --time=15:00:00
 #SBATCH --output=/gpfs/commons/home/tlin/output/sbayesR/polypred/%x_%j.log
 
-
-
 cd /gpfs/commons/home/tlin/polyfun_omer_repo
 source /gpfs/commons/groups/knowles_lab/software/anaconda3/bin/activate
 conda activate polyfun
 
 
-
-python polypred_modified.py \
-  --combine-betas \
+python polypred_modified.py --combine-betas \
   --betas /gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/max_snp_10/aggregrate.all.txt.gz,~/output/sbayesR/bellenguez_agg_snpRes \
-  --pheno /gpfs/commons/home/tlin/data/ADSP_pheno_merge_beta.tsv \
-  --output-prefix /gpfs/commons/home/tlin/output/sbayesR/mergebeta \
+  --pheno /gpfs/commons/home/tlin/data/ADSP_pheno_merge_beta_new.tsv \
+  --output-prefix /gpfs/commons/home/tlin/output/sbayesR/mergebeta_new \
   --plink-exe ~/plink /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/ADSP_chr*.bed
 
 #  --betas ~/output/sbayesR/bellenguez_agg_snpRes,/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/max_snp_10/aggregrate.all.txt.gz \
