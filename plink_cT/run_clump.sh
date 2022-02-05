e8b1aa9859ebdf2f2e49be5895cf743ebc86ddb9
Teresa Lin <tlin@nygenom.org>
2022-02-05 15:11:48 -0500

    rerun_clumping with bellenguez updated RSID

--- plink_cT/run_clump.sh
+++ plink_cT/run_clump.sh
@@ -3,7 +3,7 @@
 for i in {1..22}
 do
    echo "run chr $i"
-   sbatch --export=chr=$i plink_clump.sh
+   sbatch --export=chr=$i /gpfs/commons/home/tlin/script/plink_cT/plink_clump.sh
    #sbatch --export=chr=$i /gpfs/commons/home/tlin/polyfun_script/test/clumping_height.sh
 done
 
