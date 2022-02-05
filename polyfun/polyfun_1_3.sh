aafcc4c3b07d400f38db6dbc3435a85133540d23
teresa_sansan <joygirlsansan@gmail.com>
2022-02-05 13:55:50 -0500

    rerun bellenguez RSID updated polyfun

--- polyfun/polyfun_1_3.sh
+++ polyfun/polyfun_1_3.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=200G
 #SBATCH --time=08:00:00
-#SBATCH --output=/gpfs/commons/home/tlin/output/wightman/%x_%j.log
+#SBATCH --output=/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/%x_%j.log
 
 
 cd ~/polyfun_omer_repo
