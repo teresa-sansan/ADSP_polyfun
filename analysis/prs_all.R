6371e067bf811c111a43673943950e41cd664399
teresa_sansan <joygirlsansan@gmail.com>
2022-02-06 18:41:17 -0500

    plotting

--- analysis/prs_all.R
+++ analysis/prs_all.R
@@ -9,12 +9,12 @@ library(tibble)
 library(readr)
 
 
-## first, look at the composition of ADSP phenotype file (in plinl format)
+## first, look at the composition of ADSP phenotype file (in plink format) ----
 
-pT <- read.delim("/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/pT/pT_PRS_withPC.tsv")
+pT <- pre_process("/gpfs/commons/home/tlin/output/prs/bellenguez_pT_PRS_withPC.tsv")
 pheno_all <- read.delim("/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/phenotype_data_10_28_2021/all_phenotypes_unique_ancestry_all_all.tsv")
 pheno_subset <- read.delim("/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/phenotype_data_10_28_2021/all_phenotypes_unique_ancestry_subset.tsv")
-pT <- pre_process("/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/pT/pT_PRS_withPC.tsv")
+
 pheno_all %>% count(AD_status_final)
 pheno_subset %>% count(AD_status_final)
 pheno_subset %>% count(age_covariate)
@@ -22,10 +22,39 @@ pT %>% count(final_population)
 pT[pT$final_population=="EUR",]%>% count(Diagnosis)
 
 
+## load PRS data ----
+sbayesR <- read.delim("/gpfs/commons/home/tlin/output/prs/sbayesR.tsv")  
+names(sbayesR)[names(sbayesR) == 'age_covariate'] <- "Age"
+names(sbayesR)[names(sbayesR) == 'AD_status_final'] <- "Diagnosis"
+sbayesR = pre_process(sbayesR, FILE = TRUE)
+  ##13265 -> 11721
 
-updatePRS1 = pre_process("/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_1_subset.tsv")
-updatePRS3 = pre_process("/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_3_subset.tsv")
-updatePRS5 = pre_process("/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_5_subset.tsv")
-updatePRS7 = pre_process("/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_7_subset.tsv")
-updatePRS10 = pre_process("/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_10_subset_new.tsv")
+density_plot <- function(polypred, name){
+  sort = polypred[order(polypred$PRS),]$PRS
+  plot(density(polypred[polypred$Diagnosis == 1,]$PRS),col = "red", main=name, xlab="PRS") 
+  lines(density(polypred[polypred$Diagnosis == 0,]$PRS), col = "blue")
+  legend("topright", legend=c("Case", "Control"),
+         col=c("red", "blue"), lty=1:1, cex=0.8,
+         box.lty=0)
+}
+
+density_plot(sbayesR, name = "sbayesR")
+
+par(mfrow=c(2,3))
+
+p_threshold_col <- c("PRS_005","PRS_05","PRS_1","PRS_5")
+pT_subset = pT[p_threshold_col]
+
+for(i in c(33,35,37,34,36,38)) {       # for-loop over columns
+  plot(density((pT[, i])),col='grey', lty = 2, lwd = 2,main=str_replace(colnames(pT)[i],"PRS_", "p=0."), xlab='PRS')
+  #plot(density(pT[pT$Diagnosis == 1,i]), col = 'red',main=str_replace(colnames(pT)[i],"PRS_", "p = 0."), xlab='PRS')
+  lines(density(pT[pT$Diagnosis == 1,i]), col = 'red')
+  lines(density(pT[pT$Diagnosis == 0,i]), col = "blue")
+  legend("topright", legend=c("ALL","Case", "Control"),
+         col=c("darkgrey","red", "blue"), lty=c(2,1,1), cex=0.8,
+         box.lty=0)
+}
+
+
+
 
