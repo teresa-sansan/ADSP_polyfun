ebf21e788b22b3951cd3db68288a08d5c95d7d2b
teresa_sansan <joygirlsansan@gmail.com>
2022-02-08 22:19:03 -0500

    rerun bellenguez update RSID

--- polyfun/aggregate_finemap_result.sh
+++ polyfun/aggregate_finemap_result.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=100G
 #SBATCH --time=3:00:00
-#SBATCH --output=/gpfs/commons/home/tlin/output/wightman/finemap/%x_%j.log
+#SBATCH --output=/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/%x_%j.log
 
 
 
@@ -13,22 +13,25 @@ cd /gpfs/commons/home/tlin/polyfun_omer_repo
 source /gpfs/commons/groups/knowles_lab/software/anaconda3/bin/activate
 conda activate polyfun
 
-wightman='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/wightman_2021/wightman_2021_fixed.parquet'
-prefix='finemap_wightman'
-path='/gpfs/commons/home/tlin/output/wightman/finemap'
+bellenguez='/gpfs/commons/home/tlin/data/bellenguez_munged.parquet'
+path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap'
+prefix='finemap_bellenguez'
+
+##wightman
+#wightman='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/wightman_2021/wightman_2021_fixed.parquet'
+#prefix='finemap_wightman'
+#path='/gpfs/commons/home/tlin/output/wightman/finemap'
 
 #kunkle='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/kunkle_2019/AD_Kunkle_etal_Stage1.parquet'
 #bellenguez='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/Bellenguez_2021_stage1.parquet'
 #bellenguez='/gpfs/commons/home/tlin/data/bellenguez_2021_final.tsv.gz'
-#path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap'
-#prefix='finemap_bellenguez'
 
 
 for chr in {1..22}
 do
 	python aggregate_finemapper_results_modified.py \
 		--out-prefix $path/max_snp_${max_snp}/$prefix \
-		--sumstats $wightman \
+		--sumstats $bellenguez \
 		--out $path/max_snp_${max_snp}/chr${chr}.aggregrate.all.txt.gz \
 		--allow-missing \
        		--chr $chr 
