bdd2a4b4547cacce4b1c7d0596ba05a939e72a77
Teresa Lin <tlin@nygenom.org>
2022-02-15 17:27:11 -0500

    add MAF filter

--- data_processing/qc/bellenguez_qc.sh
+++ data_processing/qc/bellenguez_qc.sh
@@ -1,20 +1,27 @@
 cd /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021
 
+## remove SNPs MAF < 0.01
+echo 'filtering out MAF < 0.01'
+
+zcat bellenguez_2021_final_rename.tsv.gz | awk 'NR==1 || ($6>=0.01) {print}' > bellenguez_2021_qc.tsv
+ori=$(zcat bellenguez_2021_final_rename.tsv.gz | wc -l)
+remove_maf=$(cat bellenguez_2021_qc.tsv| wc -l)
+echo $(expr $ori - $remove_maf) 'of SNPs that MAF < 0.01 were removed'
+echo
+
 ## duplicated SNPs
 echo "removing duplicated SNPs..."
-ori=$(zcat bellenguez_2021_final_rename.tsv.gz | wc -l)
-zcat bellenguez_2021_final_rename.tsv.gz | awk '{seen[$3]++; if(seen[$3]==1){print}}' > bellenguez_2021_nodup.tsv
+cat bellenguez_2021_qc.tsv | awk '{seen[$3]++; if(seen[$3]==1){print}}' > bellenguez_2021_nodup.tsv
 nodup=$(cat bellenguez_2021_nodup.tsv| wc -l)
-echo $(expr $ori - $nodup) duplicated SNPs were removed
+echo $(expr $remove_maf - $nodup) duplicated SNPs were removed
 echo 
 
 ## Ambiguous SNPs
 
 echo "Removing ambiguous SNPs..."
-cat bellenguez_2021_nodup.tsv |awk '!(($4=="A" && $5 == "T") || \
- ($4=="T" && $5 == "A") || ($4=="C" && $5 == "G") ||($4=="G" && $5 == "C")) {print}' |gzip > bellenguez_2021_final_rename_qc.gz 
-
+cat bellenguez_2021_nodup.tsv |awk '!(($4=="A" && $5 == "T") || ($4=="T" && $5 == "A") || ($4=="C" && $5 == "G") ||($4=="G" && $5 == "C")) {print}' >  bellenguez_2021_final_qc 
+gzip bellenguez_2021_final_qc
 gzip bellenguez_2021_nodup.tsv
-no_ambiguous=$(zcat bellenguez_2021_final_rename_qc.gz | wc -l )
-echo $(expr $nodup - $no_ambiguous) ambiguous SNPs were removed
+no_ambiguous=$(zcat bellenguez_2021_final_rename_qc.gz | wc -l)
+echo $(expr $nodup - $no_ambiguous) 'ambiguous SNPs were removed'
 echo total number of SNPs = $(expr $no_ambiguous - 1)
