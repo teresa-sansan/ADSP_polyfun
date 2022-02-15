4315332d59f694b6dfba24b96573d16c3405ed88
Teresa Lin <tlin@nygenom.org>
2022-02-15 17:22:03 -0500

    rerun with QCed data

--- polyfun/kunkle_all_2.sh
+++ polyfun/kunkle_all_2.sh
@@ -6,23 +6,15 @@
 #SBATCH --time=10:00:00
 #SBATCH --output=/gpfs/commons/home/tlin/output/bellenguez/bellenguez_qc/%x%j.log
 
-summary_stats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/bellenguez_2021_final_rename_qc.gz'
-output='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_qc/bellenguez'
+summary_stats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/kunkle_2019/Kunkle_etal_Stage1_qc.gz'
+output='/gpfs/commons/home/tlin/output/kunkle/kunkle_qc/kunkle'
 
-#summary_stats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/Bellenguez_2021_stage1.parquet'
-#summary_stats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/bellenguez_2021_final_rename.tsv'
-#summary_stats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/bellenguez_2021_final.tsv.gz'
-#summary_stats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/wightman_2021/wightman_2021_fixed.parquet'
-#output='/gpfs/commons/home/tlin/output/wightman/wightman_all'
 
 bl='/gpfs/commons/groups/knowles_lab/data/ldsc/polyfun/baselineLF2.2.UKB'
 all_anno='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/annotations/combined_AD_annotations_polyfun/combined_AD_annotations_polyfun_'
 brain_H3K4me3='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/annotations/brain_H3K4me3/merged_annotations_ukb/brain_H3K4me3_seq_chr'
 brain_H3K27ac='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/annotations/brain_H3K27ac/merged_annotations_ukb/brain_H3K27ac_seq_chr'
 
-#output='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/bellenguez_all'
-#summary_stats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/kunkle_2019/AD_Kunkle_etal_Stage1.parquet'
-#output='/gpfs/commons/home/tlin/output/kunkle_all_2/all_anno'
 
 
 source /gpfs/commons/groups/knowles_lab/software/anaconda3/bin/activate
