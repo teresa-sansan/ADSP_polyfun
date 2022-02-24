0f0ddfa55fd330d2d47689cdb1e88276675beceb
teresa_sansan <joygirlsansan@gmail.com>
2022-02-24 16:25:33 -0500

    rename repo

---
+++ plink/merge_singlePRS_with_diagnosis.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+
+pre_file_repo='/gpfs/commons/home/tlin/output/kunkle/kunkle_qc/'
+file_name='2SNP_qc.prs.profile'
+save_name='kunkle/kunkle_2snp'
+
+
+os.system("cd /gpfs/commons/home/tlin/output/kunkle/kunkle_qc/")
+os.system("echo trimming duplicated spaces!")
+os.system("cat 2SNP_qc.prs.profile|tr -s ' ' > no_dup_space.prs")
+
+
+prs= pd.read_csv('no_dup_space.prs', sep = ' ', names = ["FID","IID","PHENO","CNT","CNT2","PRS"])
+
+pheno = pd.read_csv("/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_10_subset.tsv", sep='\t')
+
+pheno_merge = pd.merge(pheno.drop(columns = "PRS"),prs[["IID","PRS"]], left_on ="SampleID", right_on='IID')
+save="/gpfs/commons/home/tlin/output/prs/"+ save_name+'.tsv'
+pheno_merge.to_csv(save,index = False, sep='\t')
