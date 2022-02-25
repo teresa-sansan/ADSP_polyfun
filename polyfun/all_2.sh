a256bfa4d6b8a97d60dc6965decf3859d37184d0
Teresa Lin <tlin@nygenom.org>
2022-02-25 17:39:00 -0500

    run it with new bellenguez

--- polyfun/all_2.sh
+++ polyfun/all_2.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=polyfun_with_QCed_data
+#SBATCH --job-name=polyfun_bellenguez
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=200G
 #SBATCH --time=10:00:00
-#SBATCH --output=/gpfs/commons/home/tlin/output/bellenguez/bellenguez_qc/%x%j.log
+#SBATCH --output=/gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224/%x%j.log
 
-summary_stats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/bellenguez_2021_final_rename_qc.gz'
-output='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_qc/bellenguez'
+
+summary_stats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/Bellenguez_et_al_2021_hg37_no_dup.tsv.gz'
+##create munge
+output='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224/bellenguez'
+
+#summary_stats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/bellenguez_2021_final_rename_qc.gz'
+#output='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_qc/bellenguez'
 
 #summary_stats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/Bellenguez_2021_stage1.parquet'
 #summary_stats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/bellenguez_2021_final_rename.tsv'
@@ -32,31 +37,43 @@ cd ~/polyfun_omer_repo
 
 ## create parquet file
 
-#python munge_polyfun_sumstats.py \
-#  --sumstats $summary_stats \
-#  --out ~/data/bellenguez_munged.parquet 
+if false;then
+python munge_polyfun_sumstats.py \
+  --sumstats $summary_stats \
+  --out /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/Bellenguez_et_al_2021_hg37.munged.parquet
 
-summary_stats='/gpfs/commons/home/tlin/data/bellenguez_munged.parquet'
+summary_stats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/Bellenguez_et_al_2021_hg37.munged.parquet'
+fi
+
+summary_stats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/Bellenguez_et_al_2021_hg37.munged.parquet'
 
 ##1-2
-#python polyfun.py \
-#  --compute-h2-L2 \
-#  --output-prefix $output \
-#  --sumstats $summary_stats \
-#  --ref-ld-chr $bl/baselineLF2.2.UKB.,$all_anno,$brain_H3K4me3,$brain_H3K27ac \
-#  --w-ld-chr $bl/weights.UKB. \
-#  --allow-missing
+if true; then
+echo $summary_stats
+
+python polyfun.py \
+  --compute-h2-L2 \
+  --output-prefix $output \
+  --sumstats $summary_stats \
+  --ref-ld-chr $bl/baselineLF2.2.UKB.,$all_anno,$brain_H3K4me3,$brain_H3K27ac \
+  --w-ld-chr $bl/weights.UKB. \
+  --allow-missing
 
 #echo finish polyfun1_2
 
-#1-3
-#for i in {1..22}
-#do
-#sbatch --export=chr=$i,output=$output /gpfs/commons/home/tlin/script/polyfun/polyfun_1_3.sh  
-#done
+fi
 
+#1-3
+if true; then
+for i in {1..22}
+do
+sbatch --export=chr=$i,output=$output /gpfs/commons/home/tlin/script/polyfun/polyfun_1_3.sh  
+done
+fi
 
 #1-4
+if false; then
+
 python polyfun.py \
  	--compute-h2-bins \
     	--output-prefix $output \
@@ -65,3 +82,4 @@ python polyfun.py \
     	--allow-missing
 
 echo finish polyfun1_4
+fi
