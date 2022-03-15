c0b5482bcb86694a52de17333f25626e2677044d
teresa_sansan <joygirlsansan@gmail.com>
2022-03-15 12:01:13 -0400

    check the pipeline and see if its correct

--- polyfun/create_region_specific_job.sh
+++ polyfun/create_region_specific_job.sh
@@ -1,17 +1,20 @@
 cd /gpfs/commons/home/tlin/polyfun_omer_repo
-for max_snp in 1 3 5 7 10
+for max_snp in 10
 do
 	echo start creating jobs in max_snp_${max_snp} ...
 	for chr in {1..22}
 	do
 		python create_finemapper_jobs.py \
-    			--sumstats /gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224/bellenguez.${chr}.snpvar_constrained.gz \
+			--sumstats /gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/bellenguez_all.${chr}.snpvar_constrained.gz \
    	 		--n 487511 \
 			--method susie \
     			--max-num-causal $max_snp \
-    			--out-prefix /gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224/finemap_fixed_assertion_susie_iter/max_snp_${max_snp}/bellenguez \
-    			--jobs-file /gpfs/commons/home/tlin/script/polyfun/finemap_job/bellenguez_max_snp_${max_snp}_chr${chr}.sh &
+			--out-prefix /gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap_check_pipeline/bellenguez \
+			--jobs-file /gpfs/commons/home/tlin/script/polyfun/finemap_job/bellenguez/check/bellenguez_max_snp_${max_snp}_chr${chr}.sh 
+    			#--out-prefix /gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224/finemap_fixed_assertion_susie_iter/max_snp_${max_snp}/bellenguez \
+    			#--jobs-file /gpfs/commons/home/tlin/script/polyfun/finemap_job/bellenguez_max_snp_${max_snp}_chr${chr}.sh &
 
 	done
 
 done
+    			#--sumstats /gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224/bellenguez.${chr}.snpvar_constrained.gz \
--- polyfun/submit_finemap_job.sh
+++ polyfun/submit_finemap_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for max_snp in 1 3 5 7 
+for max_snp in 10
 do
 	for i in {1..22} 
 	do
 		if true; then
-		sbatch /gpfs/commons/home/tlin/script/polyfun/finemap_job/bellenguez_max_snp_${max_snp}_chr${i}.sh
+		sbatch /gpfs/commons/home/tlin/script/polyfun/finemap_job/bellenguez/check/bellenguez_max_snp_${max_snp}_chr${i}.sh
 		fi
 	done		
 done
