381ed3e2f79f42c76eac3641f95e11b43bf409d9
teresa_sansan <joygirlsansan@gmail.com>
2022-03-21 16:21:51 -0400

    deal single PRS

--- plink/merge_singlePRS_with_diagnosis.py
+++ plink/merge_singlePRS_with_diagnosis.py
@@ -3,8 +3,9 @@ import os
 
 
 #pre_file_repo='/gpfs/commons/home/tlin/output/kunkle/kunkle_qc/'
-file_name='/gpfs/commons/home/tlin/output/sbayesR/fixed_0224/sbayesR.prs'
-save_name='sbayesR_fixed_0224'
+#file_name='/gpfs/commons/home/tlin/output/sbayesR/fixed_0224/sbayesR.prs'
+file_name='/gpfs/commons/home/tlin/output/prs/bellenguez/updateRSID/interested_SNP/updateRSID_qc_interested_SNP.prs'
+save_name='/gpfs/commons/home/tlin/output/prs/bellenguez/updateRSID/interested_SNP/merged_updateRSID_qc_interested_SNP.tsv'
 
 
 #os.system("cd /gpfs/commons/home/tlin/output/kunkle/kunkle_qc/")
@@ -17,5 +18,5 @@ prs= pd.read_csv(file_name, sep = ' ', names = ["IID","PRS"])
 
 pheno = pd.read_csv("/gpfs/commons/home/tlin/output/prs/bellenguez_all_2/with_PC/UPDATEprs_diagnosis_0219.2021_max_snp_10_subset.tsv", sep='\t')
 pheno_merge = pd.merge(pheno.drop(columns = "PRS"),prs[["IID","PRS"]], left_on ="SampleID", right_on='IID')
-save="/gpfs/commons/home/tlin/output/prs/"+ save_name+'.tsv'
-pheno_merge.to_csv(save,index = False, sep='\t')
+
+pheno_merge.to_csv(save_name,index = False, sep='\t')
--- plink/prs_pT_sum.sh
+++ plink/prs_pT_sum.sh
@@ -1,4 +1,3 @@
-
 ## chr9,12,18,22 have missing file in p<5e-8
 
 #cd /gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/pT
@@ -11,5 +10,3 @@ do
 echo write pT_$i.prs
 awk '{ sum[$2]+=$6 } END {for (user in sum) print user, sum[user] }' kunkle_pT_chr*.$i.profile > pT_$i.prs
 done
-
-
