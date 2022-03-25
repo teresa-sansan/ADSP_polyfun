16c9973c9aeb16849a25940834bfd6a710ef6376
Teresa Lin <tlin@nygenom.org>
2022-03-25 15:51:55 -0400

    aggregate by max PIP

--- polyfun/aggregate_finemap_result.sh
+++ polyfun/aggregate_finemap_result.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=aggregrate_result
+#SBATCH --job-name=aggregrate_result_testmin
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=100G
 #SBATCH --time=3:00:00
-#SBATCH --output=/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap_susie/%x_%j.log
+#SBATCH --output=/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/max_snp_10/%x_%j.log
 
 cd /gpfs/commons/home/tlin/polyfun_omer_repo
 
@@ -20,9 +20,9 @@ conda activate polyfun
 ##bellenguez_updateRSID
 bellenguez='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/bellenguez_munged.parquet'
 
-prefix='bellenguez_susie'
+prefix='finemap_bellenguez'
 #path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224/finemap'
-path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap_susie'
+path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap'
 
 ##wightman
 #wightman='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/Wightman_et_al_2021_hg37_ldsc.tsv.gz'
@@ -36,10 +36,11 @@ kunkle='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzhei
 
 for chr in {1..22}
 do
-	python aggregate_finemapper_results_modified.py \
+	#python aggregate_finemapper_results_modified.py \
+	python aggregate_finemapper_results_min.py \
 		--out-prefix $path/max_snp_${max_snp}/$prefix \
 		--sumstats $bellenguez \
-		--out $path/max_snp_${max_snp}/chr${chr}.aggregrate.all.txt.gz \
+		--out $path/max_snp_${max_snp}/min_chr${chr}.aggregrate.all.txt.gz \
 		--allow-missing \
        		--chr $chr
 
--- polyfun/merge_aggregrate_finemap.sh
+++ polyfun/merge_aggregrate_finemap.sh
@@ -1,13 +1,13 @@
 #path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_bl/finemap_susie/'
 #path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_all_2/finemap_snpvar_constrained/'
-#path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/'
+path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap/'
 #path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224/finemap/'
-path='/gpfs/commons/home/tlin/output/kunkle/kunkle_fixed_0224/finemap/'
+#path='/gpfs/commons/home/tlin/output/kunkle/kunkle_fixed_0224/finemap/'
 
 
 summarystat=bellenguez
 
-for max_snp in 1 3 5 7 10
+for max_snp in 10
 do 
   echo max_snp${max_snp}
   cd ${path}/max_snp_${max_snp}
@@ -23,12 +23,12 @@ do
   #echo finished, total line = $( zcat aggregrate.all.txt.gz | wc -l )
   
 
-  zcat chr11.aggregrate.all.txt.gz| head -n 1 > agg_kunkle_extract_1e-3.tsv
+  zcat min_chr11.aggregrate.all.txt.gz| head -n 1 > agg_min_extract_1e-3.tsv
   echo 'extracting SNP with p < 0.001 ....'
   for chr in {1..22}
   do
   echo start aggregate chr $chr
-  zcat chr${chr}.aggregrate.all.txt.gz| tail -n+2| awk '{if($9 <= 0.001) print$0}' >> agg_kunkle_extract_1e-3.tsv
+  zcat min_chr${chr}.aggregrate.all.txt.gz| tail -n+2| awk '{if($9 <= 0.001) print$0}' >> agg_min_extract_1e-3.tsv
   done
-  echo finished, total line = $( cat agg_kunkle_extract_1e-3.tsv | wc -l )
+  echo finished, total line = $( cat agg_min_extract_1e-3.tsv| wc -l )
 done
