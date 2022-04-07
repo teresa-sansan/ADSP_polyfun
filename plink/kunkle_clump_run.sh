153feac6be5e5267578d475a12819d191f283f83
teresa_sansan <joygirlsansan@gmail.com>
2022-04-06 22:29:21 -0400

    run target qc seperately with variants and individual (kunkle)

--- plink/kunkle_clump_run.sh
+++ plink/kunkle_clump_run.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=150G
 #SBATCH --time=10:00:00
-#SBATCH --output=/gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/qc_check_target/qc_on_variant/%x_%j.log
+#SBATCH --output=/gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/qc_check_target/qc_on_individual/%x_%j.log
 
 ##kunkle_QCed
 sumstats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/Kunkle_et_al_2019_hg37_ldsc_qc.tsv.gz'
@@ -13,17 +13,19 @@ sumstats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzh
 ## Pvalue, SNP
 
 ##qc 
+
+
 if true; then
 echo qc
 ~/plink \
---bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc_on_variant/ADSP_qc_chr${chr} \
+--bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc_on_individual/ADSP_qc_chr${chr} \
 --clump-p1 1 \
 --clump-r2 0.1  \
 --clump-kb 250  \
 --clump $sumstats \
 --clump-snp-field SNP \
 --clump-field P \
---out /gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/qc_check_target/qc_on_variant/kunkle_qc_both_clump_chr${chr}
+--out /gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/qc_check_target/qc_on_individual/kunkle_qc_both_clump_chr${chr}
 echo
 fi
 
@@ -47,14 +49,14 @@ sumstats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzh
 if true; then
 echo qc on target
 ~/plink \
---bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc_on_variant/ADSP_qc_chr${chr} \
+--bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc_on_individual/ADSP_qc_chr${chr} \
 --clump-p1 1 \
 --clump-r2 0.1  \
 --clump-kb 250  \
 --clump $sumstats \
 --clump-snp-field SNP \
 --clump-field P \
---out /gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/qc_check_target/qc_on_variant/kunkle_clump_chr${chr}
+--out /gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/qc_check_target/qc_on_individual/kunkle_clump_chr${chr}
 echo
 fi
 
--- plink/kunkle_fixed_0224_plink_pT.sh
+++ plink/kunkle_fixed_0224_plink_pT.sh
@@ -4,24 +4,25 @@
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=50G
 #SBATCH --time=2:00:00
-#SBATCH --output=/gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/%x_%j.log
+#SBATCH --output=/gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/qc_check_target/qc_on_individual/%x_%j.log
 
-clump_path='/gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224'
+clump_path='/gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/qc_check_target/'
 
 ##qc on both
 summary_stat='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/Kunkle_et_al_2019_hg37_ldsc_qc.tsv'
 pvalue='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/Kunkle_et_al_2019_hg37_ldsc_qc.pvalue'
-if true; then
+
+if false; then
 #--bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc/ADSP_qc_chr${chr} \
 #qc='qc'
-qc='qc_all_maf01'
-awk 'NR!=1{print $3}' $clump_path/$qc/kunkle_clump_chr${chr}.clumped > $clump_path/$qc/chr${chr}.valid.snp
+qc='qc_on_individual'
+awk 'NR!=1{print $3}' $clump_path/$qc/kunkle_qc_both_clump_chr${chr}.clumped > $clump_path/$qc/qc_both_chr${chr}.valid.snp
 ~/plink \
---bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc_maf_0.1/ADSP_qc_chr${chr} \
+--bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc_on_individual/ADSP_qc_chr${chr} \
 --score  $summary_stat 1 4 6 header \
 --q-score-range range_list.txt $pvalue \
---extract $clump_path/$qc/chr${chr}.valid.snp \
---out /gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/$qc/kunkle_pT_chr${chr}
+--extract $clump_path/$qc/qc_both_chr${chr}.valid.snp \
+--out /gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/qc_check_target/$qc/qc_all_pT_chr${chr}
 fi
 
 ##qc on base
@@ -42,17 +43,17 @@ summary_stat='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/
 pvalue='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/Kunkle_et_al_2019_hg37_ldsc.pvalue'
 if true; then
 #qc='qc_on_target'
-qc='qc_target_maf01'
+qc='qc_on_variant'
 
 awk 'NR!=1{print $3}' $clump_path/$qc/kunkle_clump_chr${chr}.clumped > $clump_path/$qc/chr${chr}.valid.snp
 #--bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc/ADSP_qc_chr${chr} \
 
 ~/plink \
---bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc_maf_0.1/ADSP_qc_chr${chr} \
+--bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc_on_individual/ADSP_qc_chr${chr} \
 --score $summary_stat 1 4 6 header \
 --q-score-range range_list.txt $pvalue \
 --extract $clump_path/$qc/chr${chr}.valid.snp \
---out /gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/$qc/kunkle_pT_chr${chr}
+--out /gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/qc_check_target/qc_on_variant/kunkle_pT_chr${chr}
 
 fi
 
