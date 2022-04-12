d2b4961f98394f32aec6eff0df8425fbb1552dc3
teresa_sansan <joygirlsansan@gmail.com>
2022-04-12 16:55:49 -0400

    check the updated fixed_0224 bellenguez (fixed with assertion error)

--- polyfun/check_IBSS_converge.sh
+++ polyfun/check_IBSS_converge.sh
@@ -1,24 +1,25 @@
 touch IBSS_not_converge.txt
 #finemap_path='/gpfs/commons/home/tlin/output/wightman/fixed_0224/finemap'
-finemap_path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224/finemap'
+#finemap_path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224/finemap'
 #finemap_path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap'
+finemap_path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224_update/finemap'
 
 cd $finemap_path
+pwd
 touch IBSS_not_converge.txt
 
 for max_snp in 1 3 5 7 10
 do
-echo max_snp $max_snp >> $finemap_path/IBSS_not_converge.txt 	
+echo max_snp $max_snp | tee -a $finemap_path/IBSS_not_converge.txt 	
 	for i in {1..22} 
 	do 
-		converge=$(cat max_snp_$max_snp/finemap_bellenguez.$i.*.gz.log| grep converge| wc -l)
+		converge=$(cat max_snp_$max_snp/bellenguez.chr$i.*.gz.log| grep converge| wc -l)
 		if [ $converge != 0 ]
 		then
-			echo chr $i, not converged = $converge >> $finemap_path/IBSS_not_converge.txt
+			echo chr $i, not converged = $converge | tee -a $finemap_path/IBSS_not_converge.txt
 		fi
   
 	done 
 
 done
 
-cat $finemap_path/IBSS_not_converge.txt
--- polyfun/extract_IBSS_converge.sh
+++ polyfun/extract_IBSS_converge.sh
@@ -1,33 +1,31 @@
 touch IBSS_not_converge.txt
 #finemap_path='/gpfs/commons/home/tlin/output/wightman/fixed_0224/finemap'
-finemap_path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224/finemap'
+#finemap_path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224/finemap'
 #finemap_path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap'
-
+finemap_path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224_updated/finemap'
 #summary_stat='wightman'
 summary_stat='bellenguez'
 cd $finemap_path
 echo check convergence | tee $finemap_path/IBSS_not_converge_list.txt
 
-for max_snp in 10
+for max_snp in 1 3 5 7 10
 do
 echo max_snp $max_snp | tee -a $finemap_path/IBSS_not_converge_list.txt 	
 	for chr in {1..22} 
 	do
-		for i in $finemap_path/max_snp_$max_snp/finemap_${summary_stat}.$chr.*.gz.log
+		for i in $finemap_path/max_snp_$max_snp/${summary_stat}.chr$chr.*.gz.log
 		do 
 		converge=$(cat $i| grep converge| wc -l)
 		if [ $converge != 0 ]
 		then
-			echo $converge fails to converge.'| tee -a  $finemap_path/IBSS_not_converge_list.txt
-			echo ' ' | tee -a $finemap_path/IBSS_not_converge_list.txt
+			#echo $converge 'fails to converge.'| tee -a  $finemap_path/IBSS_not_converge_list.txt
+			#echo ' ' | tee -a $finemap_path/IBSS_not_converge_list.txt
 			ld=$( echo $i| cut -d '/' -f 11)
-			pos=$( echo $ld| cut -d '.' -f 2-4)
-			echo chr$pos | tee -a $finemap_path/IBSS_not_converge_list.txt
+			pos=$( echo $ld| cut -d '.' -f 2-3)
+			echo $pos | tee -a $finemap_path/IBSS_not_converge_list.txt
 		fi
 		done
-  
 	done
-echo ' '| tee -a $finemap_path/IBSS_not_converge_list.txt 
+echo ' ' | tee -a $finemap_path/IBSS_not_converge_list.txt 
 
 done
-#cat $finemap_path/IBSS_not_converge_list.txt
