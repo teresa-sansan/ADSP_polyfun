fb7fa0aff43923b96a8e56d1d5733cbadfb10098
teresa_sansan <joygirlsansan@gmail.com>
2022-04-21 15:48:06 -0400

    extract interested SNP using different aggregate method

--- plink/interested_SNP_prs.sh
+++ plink/interested_SNP_prs.sh
@@ -1,16 +1,16 @@
-for chr in 2 3 4 6 7 8 11 14 15 16 17 19
+for chr in {1..22}
 do
+echo run chr $chr
+
 ~/plink \
 --bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/filt/${chr}_filt \
---score /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/Bellenguez_interested_SNP.tsv 2 4 12 header \
---out /gpfs/commons/home/tlin/output/prs/bellenguez/updateRSID_interested_SNP_chr${chr}
+--score /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/Bellenguez_qc_interested_SNP_maxPIP.tsv 2 4 12 header \
+--out /gpfs/commons/home/tlin/output/prs/bellenguez/updateRSID/interested_SNP/maxPIP/updateRSID_interested_SNP_maxPIP_chr${chr}
 
 ~/plink \
---bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc/ADSP_qc_chr${chr} \
---score /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/Bellenguez_qc_interested_SNP.tsv 2 4 12 header \
---out /gpfs/commons/home/tlin/output/prs/bellenguez/updateRSID_qc_interested_SNP_chr${chr}
-
-
+--bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/filt/${chr}_filt \
+--score /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/bellenguez_2021/Bellenguez_qc_interested_SNP_minPIP.tsv 2 4 12 header \
+--out /gpfs/commons/home/tlin/output/prs/bellenguez/updateRSID/interested_SNP/minPIP/updateRSID_interested_SNP_minPIP_chr${chr}
 
 done
 
