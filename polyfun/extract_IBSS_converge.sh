02dc0d812863ffba674f75f0860ce289ab793ccd
Teresa Lin <tlin@nygenom.org>
2022-04-22 14:23:17 -0400

    fixed extract convergence script

--- polyfun/extract_IBSS_converge.sh
+++ polyfun/extract_IBSS_converge.sh
@@ -1,31 +1,49 @@
-touch IBSS_not_converge.txt
+## Teresa Lin on 04_22_22
+## purpose: SuSiE's IBSS algorithm if not converged, will generate result that is not trust-worthy. 
+## 	    So runs a job to check how many blocks fail to converge is essential. 
+## note: remember to check finemap file name before running this. 
+
+
+
 #finemap_path='/gpfs/commons/home/tlin/output/wightman/fixed_0224/finemap'
 #finemap_path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224/finemap'
 #finemap_path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_updateRSID/finemap'
-finemap_path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224_updated/finemap'
+finemap_path='/gpfs/commons/home/tlin/output/bellenguez/bellenguez_fixed_0224/old/finemap'
 #summary_stat='wightman'
 summary_stat='bellenguez'
-cd $finemap_path
-echo check convergence | tee $finemap_path/IBSS_not_converge_list.txt
 
-for max_snp in 1 3 5 7 10
+cd $finemap_path
+echo block counts for those failed to converge > IBSS_not_converge_count.txt
+
+for max_snp in  3 5 7 10
 do
-echo max_snp $max_snp | tee -a $finemap_path/IBSS_not_converge_list.txt 	
+echo WRITE not converge list in max_snp = $max_snp | tee -a IBSS_not_converge_count.txt
+echo max_snp $max_snp | tee  $finemap_path/max_snp_$max_snp/IBSS_not_converge_list.txt 	
 	for chr in {1..22} 
-	do
-		for i in $finemap_path/max_snp_$max_snp/${summary_stat}.chr$chr.*.gz.log
+	do	
+		dont_converge='F'
+		for i in $finemap_path/max_snp_$max_snp/finemap_${summary_stat}.$chr.*.gz.log
 		do 
 		converge=$(cat $i| grep converge| wc -l)
 		if [ $converge != 0 ]
 		then
-			#echo $converge 'fails to converge.'| tee -a  $finemap_path/IBSS_not_converge_list.txt
-			#echo ' ' | tee -a $finemap_path/IBSS_not_converge_list.txt
-			ld=$( echo $i| cut -d '/' -f 11)
-			pos=$( echo $ld| cut -d '.' -f 2-3)
-			echo $pos | tee -a $finemap_path/IBSS_not_converge_list.txt
+			ld=$( echo $i| cut -d '/' -f 12)
+			pos=$( echo $ld| cut -d '.' -f 2-4)
+			echo chr$pos ';' $converge 'jobs fail to converge.'| tee -a $finemap_path/max_snp_$max_snp/IBSS_not_converge_list.txt
+			echo ' ' | tee -a $finemap_path/max_snp_$max_snp/IBSS_not_converge_list.txt
+			dont_converge='T'
 		fi
 		done
-	done
-echo ' ' | tee -a $finemap_path/IBSS_not_converge_list.txt 
+		if [ $dont_converge == 'T' ]
+		then
+		count=$(cat $finemap_path/max_snp_$max_snp/IBSS_not_converge_list.txt | grep chr$chr| wc -l)
+		echo chr$chr : $count >> IBSS_not_converge_count.txt
 
+		fi
+		
+	done
+	echo ' ' >> IBSS_not_converge_count.txt
 done
+
+
+
