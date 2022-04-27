039ad358a0a44739ff5e9cc9559254a29f2126a4
teresa_sansan <joygirlsansan@gmail.com>
2022-04-27 10:48:59 -0400

    increase memory  (ran into bus error)

--- data_processing/create_plink_from_vcf.sh
+++ data_processing/create_plink_from_vcf.sh
@@ -3,10 +3,11 @@ cd /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annota
 for i in {1..22}
 do
 echo running chr $i
-sbatch --mem=10G --wrap="~/plink --vcf ADSP_annotated_chr$i.vcf.gz --recode --double-id -allow-extra-chr --out plink/ADSP_annotated_chr$i"
+sbatch --mem=70G -c 5 --wrap="~/plink --vcf ADSP_annotated_chr$i.vcf.gz --recode --double-id -allow-extra-chr --out plink/ADSP_annotated_chr$i"
 done
 
 
 ## added two flags (--double-id & --allow_extra-chr) because
 ## there were and 
 ## Error: Invalid chromosome code 'chr17_gl000205_random' on line 1790 of .vcf file. (e.g. in chr21)
+## increase the memrory size. ( bus error)
