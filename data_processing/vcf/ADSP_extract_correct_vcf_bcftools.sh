01a62fbf5d822970d48104ab0ba1ca5df6f5145c
teresa_sansan <joygirlsansan@gmail.com>
2022-05-24 16:27:17 -0400

    vcf

--- data_processing/vcf/ADSP_extract_correct_vcf_bcftools.sh
+++ data_processing/vcf/ADSP_extract_correct_vcf_bcftools.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=check_correct_chr
-#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=tlin@nygenome.org
-#SBATCH --mem=100G
+#SBATCH --mem=150G
 #SBATCH --time=22:00:00
 #SBATCH --output=/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/correct/%x%j.log
 
 cd /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37
 
-for i in 1 2 4
+for i in {1..22}
 do
 echo start chr$i
-sbatch --mem=15G -c 10 --wrap="bcftools view ADSP_annotated_chr${i}.vcf.gz --regions chr${i} > correct/ADSP_annotated_chr${i}.correct.vcf.gz"
+sbatch --mem=15G -c 10 --wrap="bcftools view ADSP_annotated_chr${i}.vcf.gz --regions chr${i} > gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/correct/ADSP_annotated_chr${i}.correct.vcf.gz"
 done
 
 
--- data_processing/vcf/check_mismap_num.sh
+++ data_processing/vcf/check_mismap_num.sh
@@ -1,18 +1,25 @@
+#!/bin/sh 
+#SBATCH --job-name=liftover_variant_counts
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=tlin@nygenome.org
+#SBATCH --mem=100G
+#SBATCH --time=22:00:00
+#SBATCH --output=/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/only_position/%x_%j.log
+
 
 ## check position
-
 output='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/only_position/map_result_mismap.txt'
 hg38='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered/only_position/ADSP_annotated_hg38_chr'
 hg37='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/only_position/ADSP_annotated_hg37_chr'
 wrong_vcf='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/only_position/incorrect_vcf/new_ADSP_annotated_hg37_chr'
-correct_vcf='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/only_position/correct_vcf/ADSP_annotated_hg37_chr'
+correct_vcf='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/correct/ADSP_annotated_chr'
 
 for i in {1..22}
 do
 echo checking chr ${i} ....
 ori=$(cat ${hg38}$i.vcf.tsv|wc -l)
 liftover=$(cat ${hg37}$i.vcf.tsv| wc -l)
-correct_map=$(cat ${correct_vcf}${i}_correct.vcf.tsv | wc -l)
+correct_map=$(cat ${correct_vcf}${i}.correct.vcf.gz | cut -f 1|wc -l)
 wrong=$(cat ${wrong_vcf}${i}_incorrect.vcf.tsv | wc -l )
 
 percent1=$(awk 'BEGIN{printf "%.2f%\n",('$liftover'/'$ori')*100}')
@@ -20,8 +27,8 @@ percent2=$(awk 'BEGIN{printf "%.2f%\n",('$correct_map'/'$liftover')*100}')
 percent3=$(awk 'BEGIN{printf "%.2f%\n",('$wrong'/'$liftover')*100}')
 
 echo chr $i in hg38 has $ori SNPs, $liftover " ("$percent1")"  left after liftover. |tee -a  $output
-#echo Where $correct_map "("$percent2 ")" SNPs are correctly mapped in the same chromosome. | tee -a $output
-echo Where $wrong "("$percent3 ")" SNPs are incorrectly mapped in the chr $i. | tee -a $output
+echo Where $correct_map "("$percent2 ")" SNPs are correctly mapped in the same chromosome. | tee -a $output
+echo aka. there are $wrong "("$percent3 ")" SNPs incorrectly mapped in the chr $i. | tee -a $output
 echo |tee -a $output
 
 done
