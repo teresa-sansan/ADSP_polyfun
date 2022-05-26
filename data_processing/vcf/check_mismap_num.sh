e9231c574a1fbdc60f6c50c71d04299074a00193
teresa_sansan <joygirlsansan@gmail.com>
2022-05-26 10:23:29 -0400

    correct vcf count

--- data_processing/vcf/check_mismap_num.sh
+++ data_processing/vcf/check_mismap_num.sh
@@ -8,14 +8,13 @@
 
 
 ## check position
-output='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/only_position/map_result_mismap.txt'
+output='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/only_position/map_result_mismap_chr${i}.txt'
 hg38='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered/only_position/ADSP_annotated_hg38_chr'
 hg37='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/only_position/ADSP_annotated_hg37_chr'
 wrong_vcf='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/only_position/incorrect_vcf/new_ADSP_annotated_hg37_chr'
 correct_vcf='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/correct/ADSP_annotated_chr'
 
-for i in {1..22}
-do
+
 echo checking chr ${i} ....
 ori=$(cat ${hg38}$i.vcf.tsv|wc -l)
 liftover=$(cat ${hg37}$i.vcf.tsv| wc -l)
@@ -28,7 +27,7 @@ percent3=$(awk 'BEGIN{printf "%.2f%\n",('$wrong'/'$liftover')*100}')
 
 echo chr $i in hg38 has $ori SNPs, $liftover " ("$percent1")"  left after liftover. |tee -a  $output
 echo Where $correct_map "("$percent2 ")" SNPs are correctly mapped in the same chromosome. | tee -a $output
-echo aka. there are $wrong "("$percent3 ")" SNPs incorrectly mapped in the chr $i. | tee -a $output
+echo There are $wrong "("$percent3 ")" SNPs incorrectly mapped in the chr $i. | tee -a $output
 echo |tee -a $output
 
-done
+
