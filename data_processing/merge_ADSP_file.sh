ffdb6c4f91a3ae65cbe48b6b5006112f4d16aef6
teresa_sansan <joygirlsansan@gmail.com>
2022-06-02 14:50:40 -0400

    merge vcf track

--- data_processing/merge_ADSP_file.sh
+++ data_processing/merge_ADSP_file.sh
@@ -2,24 +2,21 @@
 #SBATCH --job-name=merge_ADSP_Integrate_one_plink
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=tlin@nygenome.org
-#SBATCH --mem=150G
+#SBATCH --mem=100G
 #SBATCH --time=20:00:00
 #SBATCH --output=/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/%x_%j.log
 
-##no_qc
 ## setting missing variant id because 
 ## Warning: Multiple chromosomes seen for variant '.'.
-## 	--set-missing-var-ids "." \ there were errors
-## 	tried 	--set-missing-var-ids @:#"." \
-
+## 	--set-missing-var-ids "." \  ; try fixing the error with this tag but still wasn't solved.
+## 	tried 	--set-missing-var-ids @:#"." \ ; still, doesn't work
 ## 	--flip /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/ADSP_annotated-merge.missnp \
-##	--exclude /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/ADSP_annotated-merge.triallele.txt \ 
+#	--exclude-snp /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/exclude_SNP.txt \
+#	--allow-no-sex \
   
 
-~/plink --allow-no-sex \
-	--bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/ADSP_annotated_chr1 \
-	--merge-list /gpfs/commons/home/tlin/data/ADSP_merge_list.txt \
+~/plink	--bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/ADSP_annotated_chr12 \
+	--merge-list /gpfs/commons/home/tlin/data/tail_ADSP_merge_list.txt \
+	--exclude /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/exclude_SNP.txt \
 	--make-bed \
-	--exclude /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/ADSP_annotated-merge_extract.txt \
 	--out /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/ADSP_annotated_fixed
-
