24a7bbc10ff3e62d55e06e07120499b23518b6da
teresa_sansan <joygirlsansan@gmail.com>
2022-06-17 10:54:36 -0400

    qc on new target ADSP file

--- data_processing/qc/target.sh
+++ data_processing/qc/target.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-#SBATCH --job-name=target_qc_step1
+#SBATCH --job-name=target_qc
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=150G
 #SBATCH --time=22:00:00
-#SBATCH --output=/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/check/%x_%j.log
+#SBATCH --output=/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/%x_%j.log
 
-for i in {1..22}
-do
+#for i in {1..22}
+#do
+#  --bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/ADSP_chr$i \
+
 ~/plink \
-  --bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/ADSP_chr$i \
-  --maf 0.01 \
+  --bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/ADSP_annotated_fixed \
+  --maf 0.001 \
   --geno 0.01 \
   --mind 0.01 \
   --make-bed \
-  --out /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/check/ADSP_chr$i
+  --out /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/ADSP_qc_all
 
-done
+#done
 
 #  --hwe 1e-6 \ ## Didn't include. Because we dont know which sample is control.
--- data_processing/qc/target_job.sh
+++ data_processing/qc/target_job.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
 #SBATCH --job-name=target_QC
-#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=150G
 #SBATCH --time=3:00:00
-#SBATCH --output=/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc_on_individual/%x_%j.log
+#SBATCH --output=/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/%x_%j.log
 
 ## Filtered on MAF, genotype missingness rate
+path='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink'
 
 ~/plink \
-  --bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/filt/${i}_filt \
+  --bfile $path/ADSP_annotated_fixed \
   --maf 0.001 \
   --geno 0.01 \
   --make-bed \
-  --out /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc_on_variant/ADSP_qc_chr$i
+  --out $path/ADSP_qc_variant
 
 #  --hwe 1e-6 \ ## Didn't include. Because we dont know which sample is control.
 #  --mind 0.01 \ ## removed, because we want to test out how the performance is when only considerating variants. 
 
 
+
+~/plink \
+  --bfile $path/ADSP_annotated_fixed \
+  --maf 0.001 \
+  --geno 0.01 \
+  --mind 0.01 \ 
+  --make-bed \
+  --out $path/ADSP_qc_target
+
+## qc on individual alone
+if false; then 
 ~/plink \
   --bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/filt/${i}_filt \
   --mind 0.01 \
   --make-bed \
   --out /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc_on_individual/ADSP_qc_chr$i
-
+fi
 
 
 ## Remove highly correlated SNPs
