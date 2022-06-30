c26bab20bdbcf27fd547b748f64d2334dc7ae168
teresa_sansan <joygirlsansan@gmail.com>
2022-06-30 14:22:28 -0400

    clump,pT

--- plink/prs_pT_sum.sh
+++ plink/prs_pT_sum.sh
@@ -8,22 +8,30 @@
 #path='/gpfs/commons/home/tlin/output/cT/bellenguez/fixed_0224'
 #path='/gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224'
 #path='/gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/qc_check_target'
-path='/gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/subsets'
+#path='/gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/subsets'
 
 #for qc in before_qc qc_on_base qc_on_target qc
 #for qc in qc_all_maf01 qc_target_maf01	
 
 #for qc in qc_on_individual qc_on_variant
 #for path in /gpfs/commons/home/tlin/output/cT/wightman /gpfs/commons/home/tlin/output/cT/bellenguez/fixed_0224 /gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224
-for qc in before_qc qc_on_variant_sumstat
-do	
+#for qc in before_qc qc_on_variant_sumstat
+#for dir in kunkle wightman
+for dir in bellenguez
+do
+cd /gpfs/commons/home/tlin/output/cT/genomewide_plink/$dir
+ for qc in ADSP ADSP_qc_all ADSP_qc_variant ADSP_UKBB ADSP_UKBB_qc
+ do	
 
-	cd $path/$qc
+	#cd $path/$qc
+ 	cd  /gpfs/commons/home/tlin/output/cT/genomewide_plink/$dir/$qc
+	pwd
 	for i in e-5 0.001 0.005 0.01 0.05 0.1 0.5
 	do
 	echo write pT_$i.prs
 	awk '{ sum[$2]+=$6 } END {for (user in sum) print user, sum[user] }' *.$i.profile > pT_${i}.prs
 	done
 	echo 
+ done
 done
 echo finish summing up all PRS!
--- plink/run_pT_genomewide.sh
+++ plink/run_pT_genomewide.sh
@@ -12,10 +12,10 @@
 #sbatch --export=sumstats=wightman,sumstats_path_qc='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/wightman_qc.tsv' /gpfs/commons/home/tlin/script/plink/clump_genomewide.sh
 
 
-for i in {1..21}
-
+#for i in 2 4 5 6 7 8 9 10 16 17 ## wightman
+for i in 2 4 #bellenguez
 do
-sbatch --export=chr=$i,sumstats=kunkle,sumstats_path='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/Kunkle_et_al_2019_hg37_ldsc_qc.tsv',beta=6,pvalue='Kunkle_et_al_2019_hg37_ldsc_qc.pvalue' /gpfs/commons/home/tlin/script/plink/pT.sh
+#sbatch --export=chr=$i,sumstats=kunkle,sumstats_path='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/Kunkle_et_al_2019_hg37_ldsc_qc.tsv',beta=6,pvalue='Kunkle_et_al_2019_hg37_ldsc_qc.pvalue' /gpfs/commons/home/tlin/script/plink/pT.sh
 sbatch --export=chr=$i,sumstats=bellenguez,sumstats_path='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/Bellenguez_et_al_2021_hg37_qc.tsv',beta=7,pvalue='Bellenguez_et_al_2021_hg37_qc.pvalue' /gpfs/commons/home/tlin/script/plink/pT.sh
-sbatch --export=chr=$i,sumstats=wightman,sumstats_path='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/wightman_withbeta_qc.tsv',beta=9,pvalue='wightman_withbeta_qc.pvalue' /gpfs/commons/home/tlin/script/plink/pT.sh
+#sbatch --export=chr=$i,sumstats=wightman,sumstats_path='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/wightman_withbeta_qc.tsv',beta=9,pvalue='wightman_withbeta_qc.pvalue' /gpfs/commons/home/tlin/script/plink/pT.sh
 done  
