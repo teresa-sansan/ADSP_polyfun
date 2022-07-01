168e38aa039deebd1f6b94caf8e71316ac89e0c9
teresa_sansan <joygirlsansan@gmail.com>
2022-07-01 15:35:28 -0400

    pt

--- plink/merge_pT_diagnosis.py
+++ plink/merge_pT_diagnosis.py
@@ -22,21 +22,29 @@ save_name='kunkle/fixed_0224/remove_APOE_qc_on_variant_sumstat.tsv'
 #path='/gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/new_beta/'
 #save_name='kunkle/fixed_0224/new_beta_noqc'
 
-prs_e5 = pd.read_csv(path+"pT_e-5.prs", sep = ' ', names = ["IID","PRS"])
-prs_001 = pd.read_csv(path+"pT_0.001.prs", sep = ' ', names = ["IID","PRS"])
-prs_005 = pd.read_csv(path+"pT_0.005.prs", sep = ' ', names = ["IID","PRS"])   
-prs_01 = pd.read_csv(path+"pT_0.01.prs", sep = ' ', names = ["IID","PRS"]) 
-prs_05 = pd.read_csv(path+"pT_0.05.prs", sep = ' ', names = ["IID","PRS"])
-prs_1 = pd.read_csv(path+"pT_0.1.prs", sep = ' ', names = ["IID","PRS"]) 
-prs_5 = pd.read_csv(path+"pT_0.5.prs", sep = ' ', names = ["IID","PRS"]) 
 
+sumstat=["bellenguez","kunkle","wightman"]
+plink=["ADSP","ADSP_qc_all","ADSP_qc_variant","ADSP_UKBB","ADSP_UKBB_qc"]
+for x in sumstat:
+ for y in plink:
+  path = '/gpfs/commons/home/tlin/output/cT/genomewide_plink/'+ x + '/' + y + '/'
+  
+  save_name = x + '/' + x + "_" + y 
+  prs_e5 = pd.read_csv(path+"pT_e-5.prs", sep = ' ', names = ["IID","PRS"])
+  prs_001 = pd.read_csv(path+"pT_0.001.prs", sep = ' ', names = ["IID","PRS"])
+  prs_005 = pd.read_csv(path+"pT_0.005.prs", sep = ' ', names = ["IID","PRS"])   
+  prs_01 = pd.read_csv(path+"pT_0.01.prs", sep = ' ', names = ["IID","PRS"]) 
+  prs_05 = pd.read_csv(path+"pT_0.05.prs", sep = ' ', names = ["IID","PRS"])
+  prs_1 = pd.read_csv(path+"pT_0.1.prs", sep = ' ', names = ["IID","PRS"]) 
+  prs_5 = pd.read_csv(path+"pT_0.5.prs", sep = ' ', names = ["IID","PRS"]) 
 
-pheno = pd.read_csv("/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/phenotype_data_10_28_2021/all_phenotypes_unique_ancestry_subset.tsv", sep='\t')
-prs = [prs_e5, prs_001, prs_005, prs_01, prs_05, prs_1, prs_5]  
-from functools import reduce  
-prs_merge = reduce(lambda left, right:pd.merge(left,right,on=["IID"]),prs)
+  pheno = pd.read_csv("/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/phenotype_data_10_28_2021/all_phenotypes_unique_ancestry_subset.tsv", sep='\t')
+  prs = [prs_e5, prs_001, prs_005, prs_01, prs_05, prs_1, prs_5]  
+  from functools import reduce  
+  prs_merge = reduce(lambda left, right:pd.merge(left,right,on=["IID"]),prs)
 
-prs_merge = prs_merge.set_axis( ["IID","PRS_e5","PRS_001","PRS_005","PRS_01","PRS_05","PRS_1","PRS_5"], axis='columns')
-all_merge = pd.merge(pheno,prs_merge, left_on ="SampleID", right_on='IID')
-save="/gpfs/commons/home/tlin/output/prs/"+ save_name+'.tsv'
-all_merge.to_csv(save,index = False, sep='\t')
+  prs_merge = prs_merge.set_axis( ["IID","PRS_e5","PRS_001","PRS_005","PRS_01","PRS_05","PRS_1","PRS_5"], axis='columns')
+  all_merge = pd.merge(pheno,prs_merge, left_on ="SampleID", right_on='IID')
+  save="/gpfs/commons/home/tlin/output/prs/"+ save_name+'.tsv'
+  all_merge.to_csv(save,index = False, sep='\t')
+  print("save prs to " + save)
