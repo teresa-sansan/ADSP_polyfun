dfb241768ecd6804b76edd7a3aec57ba1a6a5122
teresa_sansan <joygirlsansan@gmail.com>
2022-07-07 11:04:53 -0400

    solved convergence issue by capturing tail then head. add "no replicated region" checl

--- polyfun/finemap_fixed_convergence.sh
+++ polyfun/finemap_fixed_convergence.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=dl_anno_fixed_convergence_issue
+#SBATCH --job-name=dl_anno_convergence_issue
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=150G
 #SBATCH --time=100:00:00
 #SBATCH --output=/gpfs/commons/home/tlin/output/wightman/fixed_0224/finemap/max_snp_10/try_rescue_not_converge/%x_%j.log
 
+
+## change #56 and add the revision in #57.
 cd /gpfs/commons/home/tlin/polyfun_omer_repo
 
 source /gpfs/commons/groups/knowles_lab/software/anaconda3/bin/activate
@@ -51,7 +53,9 @@ while IFS= read -r line
 do	
 	
 	chr=$(echo $line | cut -d '.' -f 1|sed s/chr//)
-	block_tail=$(echo $line | cut -d '.' -f 2| cut -d '_' -f 2)   ##only taking the tail 1MB, here is only taking the end of the 1MB tail
+	#block_tail=$(echo $line | cut -d '.' -f 2| cut -d '_' -f 2)   ##only taking the tail 1MB, here is only taking the end of the 1MB tail
+	block_tail=$(echo $line | cut -d '.' -f 3| cut -d ' ' -f 1)   ##only taking the tail 1MB, here is only taking the end of the 1MB tail, re-modified
+
 	block_head=$(expr $block_tail - 1000000) 
 	block_1=$(expr $block_head - 500000)
 	block_2=$(expr $block_head + 500000)
@@ -62,7 +66,7 @@ do
 	for start in  $block_1 $block_head $block_2   ## do finemap in each chunk. 
 	do
 	end=$(expr $start + 1000000)
-		if [ $start > 0 ]; then
+		if [ $start > 0 -a ! -f $output/try_rescue_not_converge/finemap_max_snp_${max_num_snp}_chr${chr}.${start}.${end}.log ]; then
 			python finemapper_max_iter_1000.py \
 			--ld $FILES/chr${chr}_${LD_start}_${LD_end} \
 			--sumstats $sumstat/wightman_all.${chr}.snpvar_constrained.gz \
