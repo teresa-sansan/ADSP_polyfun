93d99b7ddc6ab9f33a7b2537e460bf4874f22f46
teresa_sansan <joygirlsansan@gmail.com>
2022-07-12 17:43:41 -0400

    wightman new beta without clumping

--- plink/plink_wightman_pT.sh
+++ plink/plink_wightman_pT.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=pT
-#SBATCH --mail-type=FAIL
+#SBATCH --job-name=noclump_maxsnp10
+#SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=40G
-#SBATCH --time=2:00:00
-#SBATCH --output=/gpfs/commons/home/tlin/output/cT/wightman/qc_on_variant_sumstat/%x_%j.log
+#SBATCH --time=24:00:00
+#SBATCH --output=/gpfs/commons/home/tlin/output/cT/chr_sep_plink/wightman/new_beta/%x_%j.log
 
 which_sumstat='wightman'
-clump_path='/gpfs/commons/home/tlin/output/cT/wightman'
+clump_path='/gpfs/commons/home/tlin/output/cT/chr_sep_plink/wightman'
 qc_sumfile='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/wightman_withbeta_qc.tsv'
 sumfile='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/Wightman_2021_hg37_withbeta.tsv'
 
@@ -19,8 +19,29 @@ cat /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers
 cat /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/wightman_withbeta_qc.tsv | cut -f 1,7 > $qc_snp
 fi
 
-##qc on both
+## try new beta (PIP*beta)
 if true; then
+summary_stat='/gpfs/commons/home/tlin/output/wightman/fixed_0224/finemap/max_snp_10/agg_all_new_beta.tsv'  ## new summary stat.
+
+if [$chr ==1] then;
+cat $summary_stat |tail -n+2 | cut -f 2 > $clump_path/new_beta/max_snp_10.valid.snp
+cat agg_all_new_beta.tsv.gz| cut -f 2,16 > ${summary_stat}.pvalue  ## create p value file for that summary stat
+fi
+pvalue=$(echo ${summary_stat}.pvalue)
+
+#awk 'NR!=1{print $3}' $clump_path/new_beta/kunkle_${chr}.clumped > $clump_path/new_beta/kunkle_${chr}.valid.snp ## only include this line if you did clumping beforehand
+
+## didn't include --extract --q-score-range because I only want to test out the most basic PRS calculation.
+
+~/plink \
+--bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/filt/${chr}_filt \
+--score  $summary_stat 2 4 16 header \
+--out $clump_path/new_beta/max_snp_10_no_clump_pT_chr${chr}
+fi
+
+
+##qc on both
+if false; then
 #qc='qc'
 qc='qc_on_variant_sumstat'
 awk 'NR!=1{print $3}' $clump_path/$qc/${which_sumstat}_qc_on_both_clump_chr${chr}.clumped > $clump_path/$qc/chr${chr}.valid.snp
@@ -73,5 +94,5 @@ awk 'NR!=1{print $3}' $clump_path/$qc/${which_sumstat}_clump_chr${chr}.clumped >
 --out $clump_path/$qc/$which_sumstat_pT_chr${chr}
 
 fi
-##$1 for SNP_ID,$4 for effective allele info, $9 for effect size estimate 
+## SNP_ID, effective allele info, effect size estimate
 
