4b2cb46344571179febd67c9ef8b9c009235948e
teresa_sansan <joygirlsansan@gmail.com>
2022-07-14 10:37:35 -0400

    run susie,polypred

--- polypred/polypred.sh
+++ polypred/polypred.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=susie_Polypred
+#SBATCH --job-name=Polypred
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=tlin@nygenome.org
-#SBATCH --mem=100G
+#SBATCH --mem=50G
 #SBATCH --time=15:00:00
-#SBATCH --output=/gpfs/commons/home/tlin/output/kunkle/kunkle_fixed_0224/susie_finemap/polypred/%x_%j.log 
+#SBATCH --output=/gpfs/commons/home/tlin/output/wightman/fixed_0224/susie/finemap_fixed_assertion_susie_iter/max_snp_10/%x_%j.log 
 
 cd /gpfs/commons/home/tlin/polyfun_omer_repo
 source /gpfs/commons/groups/knowles_lab/software/anaconda3/bin/activate 
@@ -20,7 +20,7 @@ conda activate polyfun
 
 ##kunkle
 ##susie
-path='/gpfs/commons/home/tlin/output/kunkle/kunkle_fixed_0224/susie_finemap'
+#path='/gpfs/commons/home/tlin/output/kunkle/kunkle_fixed_0224/susie_finemap'
 #path='/gpfs/commons/home/tlin/output/kunkle/kunkle_fixed_0224/finemap'
 #output='	--output-prefix /gpfs/commons/home/tlin/output/bellen'
 #	--betas $path/max_snp_${max_snp}/agg_kunkle_extract_1e-3.tsv \
@@ -28,15 +28,22 @@ path='/gpfs/commons/home/tlin/output/kunkle/kunkle_fixed_0224/susie_finemap'
 
 
 ##wightman
-#path='/gpfs/commons/home/tlin/output/wightman/fixed_0224/finemap'
-#	--output-prefix $path/polypred/max_snp_${max_snp}.wightman_polypred.tsv \
+#path=/gpfs/commons/home/tlin/output/wightman/fixed_0224/finemap
+#	--output-prefix $path/polypred/max_snp_${max_snp}.new_beta_wightman_polypred.tsv \
+#	--betas $path/max_snp_${max_snp}/agg_all_new_beta.tsv.gz \
+#	--output-prefix $path/polypred/max_snp_${max_snp}_new_beta_polypred.tsv \
+
+path='/gpfs/commons/home/tlin/output/wightman/fixed_0224/susie/finemap_fixed_assertion_susie_iter'
 
 
+max_snp=10
 
-python re-clone/polypred.py \
+#python ~/polyfun_omer_repo/polypred_new_beta.py \
+
+python polypred.py \
 	--predict \
 	--betas $path/max_snp_${max_snp}/aggregate.all.txt.gz \
-	--output-prefix $path/polypred/max_snp_${max_snp}_kunkle_susie_polypred.tsv \
+	--output-prefix $path/polypred/max_snp_${max_snp}_polypred.tsv \
 	--plink-exe ~/plink \
 	/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/filt/*_filt.bed
 
