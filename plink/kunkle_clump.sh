79817df2f5e4a8f82b4563a90b13d3d1970a37d1
teresa_sansan <joygirlsansan@gmail.com>
2022-08-15 10:12:58 -0400

    running polyfun beta to c+pt

--- plink/kunkle_clump.sh
+++ plink/kunkle_clump.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=kunkle_clump_APOE_region
+#SBATCH --job-name=kunkle_pt_polyfun
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=150G
 #SBATCH --time=10:00:00
-#SBATCH --output=/gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/subsets/%x_%j.log
+#SBATCH --output=/gpfs/commons/home/tlin/output/cT/new_plink/kunkle/fixed_0224/polyfun_beta/%x_%j.log
 
 ##kunkle_QCed
 #sumstats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/Kunkle_et_al_2019_hg37_ldsc_qc.tsv.gz'
@@ -12,23 +12,26 @@
 
 
 ## kunkle_no_APOE
-sumstats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/Kunkle_remove_APOE_qc.tsv'
+#sumstats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/Kunkle_remove_APOE_qc.tsv'
+
+## kunkle_polyfun
+sumstats='/gpfs/commons/home/tlin/output/kunkle/kunkle_fixed_0224/finemap/max_snp_10/aggregate.all.txt.gz'
 
 ## Pvalue, SNP
-##qc 
-#--bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc/ADSP_qc_chr${chr} \
+##qc on both
+#--bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc_on_variant/ADSP_qc_chr${chr} \
 
-if false; then
+if true; then
 echo qc
 ~/plink \
---bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc_on_variant/ADSP_qc_chr${chr} \
+--bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc/ADSP_qc_chr${chr} \
 --clump-p1 1 \
 --clump-r2 0.1  \
 --clump-kb 250  \
 --clump $sumstats \
 --clump-snp-field SNP \
 --clump-field P \
---out /gpfs/commons/home/tlin/output/cT/kunkle/fixed_0224/subsets/qc_on_variant_sumstat/kunkle_no_APOE_qc_chr${chr}
+--out /gpfs/commons/home/tlin/output/cT/new_plink/kunkle/fixed_0224/polyfun_beta/kunkle_polyfun_qc_chr${chr}
 echo
 fi
 
@@ -66,7 +69,7 @@ fi
 
 sumstats='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/Kunkle_remove_APOE_qc.tsv'
 ##before qc
-if true; then
+if false; then
 echo no qc
 ~/plink \
 --bfile ~/data/biallelic/${chr}_filt \
--- plink/plink_pT.sh
+++ plink/plink_pT.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
-#SBATCH --job-name=pT
-#SBATCH --mail-type=FAIL
+#SBATCH --job-name=pT_kunkle_noapoe
+#SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=50G
-#SBATCH --time=2:00:00
-#SBATCH --output=/gpfs/commons/home/tlin/output/cT/bellenguez/fixed_0224/qc_on_variant_sumstat/%x_%j.log
+#SBATCH --time=10:00:00
+#SBATCH --output=/gpfs/commons/home/tlin/output/cT/genomewide_plink/kunkle/ADSP_no_apoe/%x_%j.log
 
-clump_path='/gpfs/commons/home/tlin/output/cT/bellenguez/fixed_0224'
-
+#clump_path='/gpfs/commons/home/tlin/output/cT/bellenguez/fixed_0224'
+clump_path='/gpfs/commons/home/tlin/output/cT/genomewide_plink/kunkle'
+processed='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed'
 ##qc on both
 if true; then
 #qc='qc'
-qc='qc_on_variant_sumstat'
-awk 'NR!=1{print $3}' $clump_path/$qc/bellenguez_qc_on_both_clump_chr${chr}.clumped > $clump_path/$qc/chr${chr}.valid.snp
+#qc='qc_on_variant_sumstat'
+qc='ADSP_no_apoe'
+for chr in {1..21}
+do
+awk 'NR!=1{print $3}' $clump_path/$qc/ADSP_no_apoe_qc_all_${chr}.clumped > $clump_path/$qc/chr${chr}.valid.snp
 ~/plink \
---bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/compact_filtered_vcf_16906/plink_biallelic/qc_on_variant/ADSP_qc_chr${chr} \
---score /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/Bellenguez_et_al_2021_hg37_qc.tsv 1 4 7 header \
---q-score-range range_list.txt /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/Bellenguez_et_al_2021_hg37_qc.pvalue \
+--bfile /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/ADSP_qc_all/ADSP_qc_all_${chr} \
+--score $processed/Kunkle_remove_APOE_qc.tsv 1 4 6 header \
+--q-score-range range_list.txt $processed/Kunkle_remove_APOE_qc.pvalue \
 --extract $clump_path/$qc/chr${chr}.valid.snp \
---out /gpfs/commons/home/tlin/output/cT/bellenguez/fixed_0224/$qc/bellenguez_pT_chr${chr}
+--out $clump_path/$qc/${qc}_qc_all_chr${chr}
+
+done
 fi
 
 ##qc on base
