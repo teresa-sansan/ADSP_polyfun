0ed95898e417cb9ff6a01b3c028475cb6dcad400
teresa_sansan <joygirlsansan@gmail.com>
2022-10-19 00:56:41 -0400

    run wightman converge

--- polypred/polypred.sh
+++ polypred/polypred.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=Polypred_bellenguez_not_fixed
+#SBATCH --job-name=Polypred_susie
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=50G
 #SBATCH --time=5:00:00
-#SBATCH --output=/gpfs/commons/home/tlin/output/bellenguez/old/bellenguez_fixed_0224/finemap/polypred_new_plink/%x_%j.log 
+#SBATCH --output=/gpfs/commons/home/tlin/output/wightman/wightman_check_1003/susie/finemap/polypred/%x_%j.log 
 
 cd /gpfs/commons/home/tlin/polyfun_omer_repo
 source /gpfs/commons/groups/knowles_lab/software/anaconda3/bin/activate 
@@ -17,7 +17,7 @@ path='/gpfs/commons/home/tlin/output/kunkle/kunkle_fixed_0224/susie_finemap'
 fi
 
 ## bellenguez
-if true; then
+if false; then
 ## susie
 #path='/gpfs/commons/home/tlin/output/bellenguez/old/bellenguez_fixed_0224_annotations/susie'
 path='/gpfs/commons/home/tlin/output/bellenguez/old/bellenguez_fixed_0224/finemap'
@@ -25,9 +25,10 @@ path='/gpfs/commons/home/tlin/output/bellenguez/old/bellenguez_fixed_0224/finema
 fi
 
 ##wightman
-if false; then
-#path='/gpfs/commons/home/tlin/output/wightman/wightman_check_1003/all_anno/finemap/'
+if true; then
 path='/gpfs/commons/home/tlin/output/wightman/wightman_check_1003/susie/finemap/'
+#path='/gpfs/commons/home/tlin/output/wightman/wightman_check_1003/bl/finemap/'
+
 fi
 
 ## jansen
@@ -36,10 +37,14 @@ path='/gpfs/commons/home/tlin/output/jansen/finemap'
 #path='/gpfs/commons/home/tlin/output/jansen/susie'
 fi
 
+
+ori_file='aggregate.all.txt'
+converge_file='agg_fixed_converge.tsv.gz'
+
 python polypred.py \
 	--predict \
-	--betas $path/max_snp_${max_snp}/aggregate.all.txt \
-	--output-prefix $path/polypred_new_plink/not_fixed_max_snp_${max_snp}_polypred.tsv \
+	--betas $path/max_snp_${max_snp}/$converge_file \
+	--output-prefix $path/polypred/fixed_max_snp_${max_snp}_polypred.tsv \
 	--plink-exe ~/plink \
 	/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/ADSP_qc_all/ADSP_qc_all_*.bed
 
