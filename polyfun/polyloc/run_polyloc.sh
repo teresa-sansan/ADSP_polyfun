642562e5d41bb3bcf89f0af9862daa95f3c47613
teresa_sansan <joygirlsansan@gmail.com>
2023-07-25 16:20:17 -0400

    polyloc analysis

--- polyfun/polyloc/run_polyloc.sh
+++ polyfun/polyloc/run_polyloc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=polyloc_stage2_
+#SBATCH --job-name=polyloc_pip0_
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=50G 
-#SBATCH --time=5:00:00
-#SBATCH --output=/gpfs/commons/home/tlin/output/wightman/new_anno_0203/update_all+enformer/finemap/polyloc/%x%j.log
+#SBATCH --time=30:00:00
+#SBATCH --output=/gpfs/commons/home/tlin/output/wightman/new_anno_0203/update_all+enformer/finemap/polyloc/pip_not0/%x%j.log
 
 cd /gpfs/commons/home/tlin/polyfun_omer_repo
 source /gpfs/commons/groups/knowles_lab/software/anaconda3/bin/activate 
@@ -12,22 +12,39 @@ conda activate polyfun
 
 polyfun_path='/gpfs/commons/home/tlin/output/wightman/new_anno_0203/update_all+enformer/finemap/'
 
-## Stage 1
+# # Stage 1
 # python polyloc_reclone/polyloc.py \
 #     --compute-partitions \
 #     --output-prefix $polyfun_path/polyloc/polyloc \
 #     --posterior $polyfun_path/max_snp_5/aggregate.all.txt \
 #     --bfile-chr /gpfs/commons/groups/knowles_lab/data/ldsc/polyfun/UKB_snp_list/UKB_SNP_list_chr
     
-## Stage 2
+# # Stage 2
+# python polyloc_reclone/polyloc.py \
+#     --output-prefix $polyfun_path/polyloc/polyloc \
+#     --compute-ldscores \
+#     --ld-ukb \
+#     --ld-dir /gpfs/commons/groups/knowles_lab/data/ldsc/polyfun/ukb_ld/ \
+#     --chr $chr
+
+## Stage 1 & 2
 python polyloc_reclone/polyloc.py \
-    --output-prefix $polyfun_path/polyloc/polyloc \
+    --compute-partitions \
+    --output-prefix $polyfun_path/polyloc/pip_not0/polyloc \
     --compute-ldscores \
+    --posterior $polyfun_path/max_snp_5/agg_extract_pip_not0.tsv \
+    --bfile-chr /gpfs/commons/groups/knowles_lab/data/ldsc/polyfun/UKB_snp_list/UKB_SNP_list_chr \
     --ld-ukb \
     --ld-dir /gpfs/commons/groups/knowles_lab/data/ldsc/polyfun/ukb_ld/ \
-    --chr $chr
 
 
+
+## Stage 3
+# python polyloc_reclone/polyloc.py \
+#     --output-prefix $polyfun_path/polyloc/polyloc \
+#     --compute-polyloc \
+#     --w-ld-chr /gpfs/commons/groups/knowles_lab/data/ldsc/polyfun/baselineLF2.2.UKB/weights.UKB. \
+#     --sumstats /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/sep22_new_bellenguez_et_al_2021_hg37_ldsc.munged.parquet
    # 
 ## add this so it wont take that much time. If dont add this tag, the default one is 29
 # 36k /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/36K_preview/plink_hg38_qc/ADSP_qc_chr 
--- polyfun/polyloc/run_polyloc_chr.sh
+++ polyfun/polyloc/run_polyloc_chr.sh
@@ -1,4 +1,4 @@
-for i in {1..21}
+for i in {19..22}
         do
                 echo "chr = $i" 
                 sbatch --export=chr=$i run_polyloc.sh 
