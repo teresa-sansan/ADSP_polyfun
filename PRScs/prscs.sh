afde2dee7f4fc3438c7618330c9a0ad9653b699c
teresa_sansan <joygirlsansan@gmail.com>
2024-09-09 16:10:05 -0400

    run ukbb for comparison

--- PRScs/prscs.sh
+++ PRScs/prscs.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=prscs
+#SBATCH --job-name=prscs_ukbb
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=tlin@nygenome.org
-#SBATCH --mem=20G
+#SBATCH --mem=30G
 #SBATCH --time=25:00:00
-#SBATCH --array=1-20%20
-#SBATCH --output=/gpfs/commons/home/tlin/output/prs/PRSCS/36k_adsp_ld_panel/bellenguez/%x_%j.log
+#SBATCH --array=1-22%10
+#SBATCH --output=/gpfs/commons/home/tlin/output/prs/PRSCS/36k/bellenguez_rerun_0909/%x_%j.log
 
 
-##recommend to run it per chr chunk. (each chr takes ~30 min). run it with run_prscs.sh directly.
-
+##recommend to run it per chr chunk. 
 
 cd /gpfs/commons/home/tlin/PRScs
 source /gpfs/commons/groups/knowles_lab/software/anaconda3/bin/activate 
@@ -23,21 +22,29 @@ sumstat='prscs/bellenguez_hg38_parquet_flipped_4prscs.tsv'
 #sumstat='prscs/bellenguez_hg38_chr21.tsv'
 
 n_sumstat=487511
-output_dir='/gpfs/commons/home/tlin/output/prs/PRSCS/36k_adsp_ld_panel/bellenguez'
+output_dir='/gpfs/commons/home/tlin/output/prs/PRSCS/36k_adsp_ld_panel/bellenguez/bellenguez'
 
-python PRScs.py --ref_dir=/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/LD/LD_ADSP36K_4PRScs_Aug/ldblk_adsp_chr \
+
+## adsp ld reference
+# python PRScs.py --ref_dir=/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/LD/LD_ADSP36K_4PRScs_Aug/ldblk_adsp_chr \
+#     --bim_prefix=/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/36K_QC/annotated_hg38_plink_qc/ADSP.chr${chr} \
+#     --sst_file=$sumstat_path/$sumstat \
+#     --chr $chr --n_gwas=$n_sumstat --phi=1e-2 \
+#     --out_dir=$output_dir
+
+
+## ukb ld reference
+python PRScs.py --ref_dir=/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/LD/LD_PRScs/ldblk_ukbb_eur \
     --bim_prefix=/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/36K_QC/annotated_hg38_plink_qc/ADSP.chr${chr} \
     --sst_file=$sumstat_path/$sumstat \
     --chr $chr --n_gwas=$n_sumstat --phi=1e-2 \
-    --out_dir=$output_dir
-
+    --out_dir=/gpfs/commons/home/tlin/output/prs/PRSCS/36k/bellenguez_rerun_0909/bellenguez
 
 ## ukb_ld /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/36K_QC/annotated_hg37_plink_ibd/qc/qc_chr${chr}
-## ukb_ref /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/LD_PRScs/ldblk_ukbb_eur
+## ukb_ref /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/LD/LD_PRScs/ldblk_ukbb_eur
 
 ## old geno
 ## /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/17K_final/annotated_filtered_hg37/plink/vcf_filt/ADSP_annotated_merged_qc
-
 
 ## /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/wightman_prscs.tsv
 ## --ref_dir: LD panel file
