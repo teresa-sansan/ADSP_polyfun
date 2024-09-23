6938feb554765e6dd7ef9a4113a0b7bff5b98db9
teresa_sansan <joygirlsansan@gmail.com>
2024-09-23 19:21:22 -0400

    extract snp with a lot of nan in LD

--- carma/count_ld_nan.sh
+++ carma/count_ld_nan.sh
@@ -1,4 +1,4 @@
-cd /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/LD/LD_CARMA
+cd /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/LD/LD_CARMA/geno_filt
 for chr in {15..22}
 do   
     n_ld=$(ls -1 | grep .bim| grep chr${chr}_ | wc -l)
@@ -6,14 +6,16 @@ do
     touch count/nan_size.txt
     for i in $(seq 1 "$n_ld")
     do
+        size_ld=$(wc -l < chr${chr}_${i}.bim)
         nan=$(cat chr${chr}_${i}.ld|grep -o nan |wc -l)
-        output=/gpfs/commons/home/tlin/output/CARMA/${chr}_${i}.txt.gz
+        output=/gpfs/commons/home/tlin/output/CARMA/geno_filt/${chr}_${i}.txt.gz
         if [ -e $output ]; then
-            result=$(ls -ll $output | awk '{print $6, $7}')
+            result='success'
         else {
            result="failed"
         }
         fi
-        echo $chr $i $nan $result | tee -a count/nan_size.txt
+        echo $chr $i $size_ld $nan $result | tee -a count/nan_sizes.txt
     done
 done
+
