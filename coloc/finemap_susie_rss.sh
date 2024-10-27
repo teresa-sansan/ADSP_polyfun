555003523b46f2dcacb2ea0bc23a08c1fc7222fb
teresa_sansan <joygirlsansan@gmail.com>
2024-10-27 18:50:27 -0400

    coloc_gwas

--- coloc/finemap_susie_rss.sh
+++ coloc/finemap_susie_rss.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=finemap_omics_test
+#SBATCH --job-name=test
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=50G
 #SBATCH --time=40:00:00
-#SBATCH --output=/gpfs/commons/home/tlin/output/36k/bellenguez/adsp_ld/susie_rss/snp_of_interest/%x_%j.log
+#SBATCH --output=/gpfs/commons/home/tlin/output/36k/bellenguez/adsp_ld/susie_rss_oct/snp_of_interest/%x_%j.log
 
 
 source /gpfs/commons/groups/knowles_lab/software/anaconda3/bin/activate
@@ -28,9 +28,9 @@ FILES="/gpfs/commons/groups/knowles_lab/data/ldsc/polyfun/ukb_ld/chr${chr}_*.npz
 sumstat_name='bellenguez_hg38'
 sumstat='bellenguez_hg38_qc.tsv'
 
-##bellenguez hg19
+#anno_path='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/ADSP_reference_panel/fine_mapping/annotations_dl/finemap_backup_teresa/bellenguez'
+anno_path='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/ADSP_reference_panel/fine_mapping/annotations_dl/teresa/backup_oct/'
 
-anno_path='/gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/ADSP_reference_panel/fine_mapping/annotations_dl/finemap_backup_teresa/bellenguez'
 n=487511
 prefix='bellenguez'
 
@@ -43,31 +43,31 @@ if [ $anno == 'susie' ]; then
 	echo run finemapping on susie
 	python /gpfs/commons/home/tlin/polyfun_omer_repo/finemapper_susie_rss.py \
 						--geno /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/1KG/plink_ADSP_filtered_deduplicated/ADSP_chr${chr} \
-						--sumstats /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/bellenguez_hg38_qc_nodup.tsv \
+						--sumstats /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/summary_stats/alzheimers/fixed_alzheimers/processed/bellenguez_hg38_parquet_flipped_a1a2.tsv \
 						--n $n --chr ${chr} --start $start --end $end \
 						--method susie \
 						--max-num-causal 10 \
 						--non-funct \
 						--no-sort-pip \
 						--susie-resvar 1 \
-						--susie-outfile /gpfs/commons/home/tlin/output/36k/bellenguez/adsp_ld/susie_rss/snp_of_interest/${anno}_$output \
+						--susie-outfile /gpfs/commons/home/tlin/output/36k/bellenguez/adsp_ld/susie_rss_oct/snp_of_interest/${anno}_$output \
 						--allow-missing \
-						--out /gpfs/commons/home/tlin/output/36k/bellenguez/adsp_ld/susie_rss/snp_of_interest/${anno}_${output}.tsv
+						--out /gpfs/commons/home/tlin/output/36k/bellenguez/adsp_ld/susie_rss_oct/snp_of_interest/${anno}_${output}.tsv
 
 
 else
 	echo run functional informed finemapping, on $anno
 	python /gpfs/commons/home/tlin/polyfun_omer_repo/finemapper_susie_rss.py \
 						--geno /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/ADSP_vcf/1KG/plink_ADSP_filtered_deduplicated/ADSP_chr${chr} \
-						--sumstats ${anno_path}_${anno}/${prefix}_${anno}.${chr}.snpvar_ridge.gz \
+						--sumstats ${anno_path}/bellenguez_${anno}.${chr}.snpvar_ridge.gz \
 						--n $n --chr ${chr} --start $start --end $end \
 						--method susie \
 						--max-num-causal 10 \
 						--no-sort-pip \
-						--susie-resvar 1 \	
-						--susie-outfile /gpfs/commons/home/tlin/output/36k/bellenguez/adsp_ld/susie_rss/snp_of_interest/${anno}_${output} \
+						--susie-resvar 1 \
 						--allow-missing \
-						--out /gpfs/commons/home/tlin/output/36k/bellenguez/adsp_ld/susie_rss/snp_of_interest/${anno}_${output}.tsv
+						--out /gpfs/commons/home/tlin/output/36k/bellenguez/adsp_ld/susie_rss_oct/snp_of_interest/${anno}_${output}.tsv \
+						--susie-outfile /gpfs/commons/home/tlin/output/36k/bellenguez/adsp_ld/susie_rss_oct/snp_of_interest/${anno}_${output} \
 
 fi
 
--- coloc/submit_finemap_gwas.sh
+++ coloc/submit_finemap_gwas.sh
@@ -3,14 +3,14 @@
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=tlin@nygenome.org
 #SBATCH --mem=30G
-#SBATCH --time=10:00:00
-#SBATCH --array=1-104%15
+#SBATCH --time=30:00:00
+#SBATCH --array=1-324%50
 
-
+#/gpfs/commons/home/tlin/script/coloc/snp_of_interest.tsv
 line=$SLURM_ARRAY_TASK_ID
 # line=1
 start=$((line +1))
-head -n $start /gpfs/commons/home/tlin/script/coloc/snp_of_interest.tsv | tail -1 | while IFS=$'\t' read -r CHR anno BP
+head -n $start /gpfs/commons/groups/knowles_lab/data/ADSP_reguloML/PRS/36k_hg38/snp_pip_thres/remove_index0/union_snp_of_interest.tsv | tail -1 | while IFS=$'\t' read -r CHR anno BP
 do
     name=${anno}_chr${CHR}_${BP}
     echo run $name
